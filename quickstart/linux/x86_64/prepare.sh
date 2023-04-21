65a32a977ff87729d0b4e8b37f758fd8dff7ca2b
jingjingxyk <zonghengbaihe521@qq.com>
2023-04-22 02:05:26 +0800

    添加快速进入编译环境脚本

--- quickstart/linux/x86_64/prepare.sh
+++ quickstart/linux/x86_64/prepare.sh
@@ -18,5 +18,8 @@ chmod +x bin/spc
 ./bin/spc fetch --all --debug
 
 ./bin/spc list-ext
-# 构建包含 bcmath,openssl,tokenizer,sqlite3,pdo_sqlite,ftp,curl 扩展的 php-cli 和 micro.sfx
-./bin/spc build "bcmath,openssl,tokenizer,sqlite3,pdo_sqlite,ftp,curl" --build-all --cc=gcc --debug
+
+
+#./bin/spc build "bcmath,openssl,tokenizer,sqlite3,pdo,pdo_sqlite,ftp,curl" --cc=gcc --cxx=g++  --debug
+
+./bin/spc build "bcmath,openssl,tokenizer,sqlite3,pdo,pdo_sqlite,ftp,curl" --cc=clang --cxx=clang++  --debug
