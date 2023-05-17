f281d5eeb7c3eef22c25fe10f10e0ab7e3d2c058
javalaw <739554820@qq.com>
2023-05-17 07:36:23 +0000

    fix libsodium.

--- src/SPC/builder/unix/library/libsodium.php
+++ src/SPC/builder/unix/library/libsodium.php
@@ -8,10 +8,11 @@ trait libsodium
 {
     protected function build()
     {
+        $root = BUILD_ROOT_PATH;
         shell()->cd($this->source_dir)
-            ->exec("{$this->builder->configure_env} ./configure --enable-static --disable-shared --prefix=")
+            ->exec("{$this->builder->configure_env} ./configure --enable-static --disable-shared --prefix={$root}")
             ->exec('make clean')
             ->exec("make -j{$this->builder->concurrency}")
-            ->exec('make install DESTDIR=' . BUILD_ROOT_PATH);
+            ->exec('make install');
     }
 }
