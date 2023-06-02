1398086c38c717fc55f96738c733a00eeac86c02
crazywhalecc <jesse2061@outlook.com>
2023-06-02 20:26:19 +0800

    add icu configure options for linux

--- src/SPC/builder/linux/library/icu.php
+++ src/SPC/builder/linux/library/icu.php
@@ -11,8 +11,22 @@ class icu extends LinuxLibraryBase
     protected function build()
     {
         $root = BUILD_ROOT_PATH;
+        $cppflag = 'CPPFLAGS="-DU_CHARSET_IS_UTF8=1  -DU_USING_ICU_NAMESPACE=1  -DU_STATIC_IMPLEMENTATION=1"';
         shell()->cd($this->source_dir . '/source')
-            ->exec("{$this->builder->configure_env} ./runConfigureICU Linux --enable-static --disable-shared --prefix={$root}")
+            ->exec(
+                "{$this->builder->configure_env} {$cppflag} ./runConfigureICU Linux " .
+                '--enable-static ' .
+                '--disable-shared ' .
+                '--with-data-packaging=static ' .
+                '--enable-release=yes ' .
+                '--enable-extras=yes ' .
+                '--enable-icuio=yes ' .
+                '--enable-dyload=no ' .
+                '--enable-tools=yes ' .
+                '--enable-tests=no ' .
+                '--enable-samples=no ' .
+                "--prefix={$root}"
+            )
             ->exec('make clean')
             ->exec("make -j{$this->builder->concurrency}")
             ->exec('make install');
