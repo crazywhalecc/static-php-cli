c49e6ba0a2a60ba86652b57cfabb64190cdba919
crazywhalecc <jesse2061@outlook.com>
2023-06-15 22:22:07 +0800

    fix libwebp version (imagemagick with webp-1.3.1-rc1 crashed)

--- src/SPC/ConsoleApplication.php
+++ src/SPC/ConsoleApplication.php
@@ -16,7 +16,7 @@ use Symfony\Component\Console\Command\ListCommand;
  */
 class ConsoleApplication extends Application
 {
-    public const VERSION = '2.0-rc1';
+    public const VERSION = '2.0-rc2';
 
     /**
      * @throws \ReflectionException
