63287dd9c4e2b565dc905c6ae0f80d0a7688260d
crazywhalecc <jesse2061@outlook.com>
2023-07-17 20:26:48 +0800

    add linux tool check

---
+++ src/SPC/doctor/item/LinuxToolCheckList.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SPC\doctor\item;
+
+use SPC\builder\linux\SystemUtil;
+use SPC\builder\traits\UnixSystemUtilTrait;
+use SPC\doctor\AsCheckItem;
+use SPC\doctor\AsFixItem;
+use SPC\doctor\CheckResult;
+use SPC\exception\RuntimeException;
+
+class LinuxToolCheckList
+{
+    use UnixSystemUtilTrait;
+
+    #[AsCheckItem('if necessary tools are installed', limit_os: 'Linux')]
+    public function checkCliTools(): ?CheckResult
+    {
+        $distro = SystemUtil::getOSRelease();
+
+        $required = match ($distro['dist']) {
+            'ubuntu', 'debian' => [
+                'make', 'bison', 'flex',
+                'git', 'autoconf', 'automake',
+                'tar', 'unzip', 'gzip',
+                'bzip2', 'cmake',
+            ],
+            'alpine' => [
+                'make', 'bison', 'flex',
+                'git', 'autoconf', 'automake',
+                'tar', 'unzip', 'gzip',
+                'bzip2', 'cmake', 'gcc',
+            ]
+        };
+        $missing = [];
+        foreach ($required as $cmd) {
+            if ($this->findCommand($cmd) === null) {
+                $missing[] = $cmd;
+            }
+        }
+        if (!empty($missing)) {
+            return match ($distro['dist']) {
+                'ubuntu', 'alpine', 'debian' => CheckResult::fail(implode(', ', $missing) . ' not installed on your system', 'install-linux-tools', [$distro, $missing]),
+                default => CheckResult::fail(implode(', ', $missing) . ' not installed on your system'),
+            };
+        }
+        return CheckResult::ok();
+    }
+
+    #[AsFixItem('install-linux-tools')]
+    public function fixBuildTools(array $distro, array $missing): bool
+    {
+        $install_cmd = match ($distro['dist']) {
+            'ubuntu', 'debian' => 'apt install -y',
+            'alpine' => 'apk add',
+            default => throw new RuntimeException('Current linux distro is not supported for auto-install musl packages'),
+        };
+        $prefix = '';
+        if (get_current_user() !== 'root') {
+            $prefix = 'sudo ';
+            logger()->warning('Current user is not root, using sudo for running command');
+        }
+        try {
+            shell(true)->exec($prefix . $install_cmd . ' ' . implode(' ', $missing));
+        } catch (RuntimeException) {
+            return false;
+        }
+        return true;
+    }
+}
