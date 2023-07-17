08efc81cf075fac957e55b47b793dc249ba3acc4
crazywhalecc <jesse2061@outlook.com>
2023-07-17 21:10:49 +0800

    add --auto-fix option

--- src/SPC/command/DoctorCommand.php
+++ src/SPC/command/DoctorCommand.php
@@ -10,11 +10,16 @@ use Symfony\Component\Console\Attribute\AsCommand;
 #[AsCommand('doctor', 'Diagnose whether the current environment can compile normally')]
 class DoctorCommand extends BaseCommand
 {
+    public function configure()
+    {
+        $this->addOption('auto-fix', null, null, 'Automatically fix failed items (if possible)');
+    }
+
     public function handle(): int
     {
         try {
             $checker = new CheckListHandler($this->input, $this->output);
-            $checker->runCheck(FIX_POLICY_PROMPT);
+            $checker->runCheck($this->input->getOption('auto-fix') ? FIX_POLICY_AUTOFIX : FIX_POLICY_PROMPT);
             $this->output->writeln('<info>Doctor check complete !</info>');
         } catch (\Throwable $e) {
             $this->output->writeln('<error>' . $e->getMessage() . '</error>');
