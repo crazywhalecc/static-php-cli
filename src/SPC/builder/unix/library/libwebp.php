2e3d4302b41def24e9c421e4f1a54c7efc789f7f
crazywhalecc <jesse2061@outlook.com>
2023-08-27 03:05:53 +0800

    fix webp compile to cmake, fix #95

--- src/SPC/builder/unix/library/libwebp.php
+++ src/SPC/builder/unix/library/libwebp.php
@@ -7,6 +7,7 @@ namespace SPC\builder\unix\library;
 use SPC\exception\FileSystemException;
 use SPC\exception\RuntimeException;
 use SPC\exception\WrongUsageException;
+use SPC\store\FileSystem;
 
 trait libwebp
 {
@@ -17,25 +18,20 @@ trait libwebp
      */
     protected function build(): void
     {
-        [,,$destdir] = SEPARATED_PATH;
-
-        shell()->cd($this->source_dir)
-            ->exec('./autogen.sh')
+        // CMake needs a clean build directory
+        FileSystem::resetDir($this->source_dir . '/build');
+        // Start build
+        shell()->cd($this->source_dir . '/build')
             ->exec(
-                "{$this->builder->configure_env} ./configure " .
-                '--enable-static ' .
-                '--disable-shared ' .
-                '--prefix= ' .
-                '--enable-libwebpdecoder ' .
-                '--enable-libwebpextras ' .
-                '--disable-tiff ' .
-                '--disable-gl ' .
-                '--disable-sdl ' .
-                '--disable-wic'
+                "{$this->builder->configure_env} cmake " .
+                $this->builder->makeCmakeArgs() . ' ' .
+                '-DBUILD_SHARED_LIBS=OFF ' .
+                '-DWEBP_BUILD_EXTRAS=ON ' .
+                '..'
             )
-            ->exec('make clean')
-            ->exec("make -j{$this->builder->concurrency}")
-            ->exec('make install DESTDIR=' . $destdir);
+            ->exec("cmake --build . -j {$this->builder->concurrency}")
+            ->exec('make install DESTDIR=' . BUILD_ROOT_PATH);
+        // patch pkgconfig
         $this->patchPkgconfPrefix(['libsharpyuv.pc', 'libwebp.pc', 'libwebpdecoder.pc', 'libwebpdemux.pc', 'libwebpmux.pc'], PKGCONF_PATCH_PREFIX);
         $this->cleanLaFiles();
     }
