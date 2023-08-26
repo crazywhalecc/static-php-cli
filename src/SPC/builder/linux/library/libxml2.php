3db78c7bce398de9109dff462aa0e5f3cd87feca
crazywhalecc <jesse2061@outlook.com>
2023-08-26 23:51:24 +0800

    fix linux libxml2 #75 issue

--- src/SPC/builder/linux/library/libxml2.php
+++ src/SPC/builder/linux/library/libxml2.php
@@ -19,21 +19,22 @@ class libxml2 extends LinuxLibraryBase
     public function build(): void
     {
         $enable_zlib = $this->builder->getLib('zlib') ? 'ON' : 'OFF';
-        $enable_icu = $this->builder->getLib('icu') ? 'ON' : 'OFF';
+        // $enable_icu = $this->builder->getLib('icu') ? 'ON' : 'OFF';
         $enable_xz = $this->builder->getLib('xz') ? 'ON' : 'OFF';
-
-        [, , $destdir] = SEPARATED_PATH;
 
         FileSystem::resetDir($this->source_dir . '/build');
         shell()->cd($this->source_dir . '/build')
             ->exec(
                 "{$this->builder->configure_env} " . ' cmake ' .
-                "{$this->builder->makeCmakeArgs()} " .
+                "-DCMAKE_TOOLCHAIN_FILE={$this->builder->cmake_toolchain_file} " .
+                '-DCMAKE_BUILD_TYPE=Release ' .
+                '-DCMAKE_INSTALL_PREFIX=' . escapeshellarg(BUILD_ROOT_PATH) . ' ' .
                 '-DBUILD_SHARED_LIBS=OFF ' .
+                '-DCMAKE_INSTALL_BINDIR=' . escapeshellarg(BUILD_ROOT_PATH . '/bin') . ' ' .
                 '-DLIBXML2_WITH_ICONV=ON ' .
                 '-DIconv_IS_BUILT_IN=OFF ' .
                 "-DLIBXML2_WITH_ZLIB={$enable_zlib} " .
-                "-DLIBXML2_WITH_ICU={$enable_icu} " .
+                '-DLIBXML2_WITH_ICU=OFF ' .
                 "-DLIBXML2_WITH_LZMA={$enable_xz} " .
                 '-DLIBXML2_WITH_PYTHON=OFF ' .
                 '-DLIBXML2_WITH_PROGRAMS=OFF ' .
@@ -41,15 +42,6 @@ class libxml2 extends LinuxLibraryBase
                 '..'
             )
             ->exec("cmake --build . -j {$this->builder->concurrency}")
-            ->exec("make install DESTDIR={$destdir}");
-
-        if (is_dir(BUILD_INCLUDE_PATH . '/libxml2/libxml')) {
-            if (is_dir(BUILD_INCLUDE_PATH . '/libxml')) {
-                shell()->exec('rm -rf "' . BUILD_INCLUDE_PATH . '/libxml"');
-            }
-            $path = FileSystem::convertPath(BUILD_INCLUDE_PATH . '/libxml2/libxml');
-            $dst_path = FileSystem::convertPath(BUILD_INCLUDE_PATH . '/');
-            shell()->exec('mv "' . $path . '" "' . $dst_path . '"');
-        }
+            ->exec('make install');
     }
 }
