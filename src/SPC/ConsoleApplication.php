54e98666e7b41917c25922311d6a9e3b144a2ffa
Jerry Ma <jesse2061@outlook.com>
2023-09-08 22:03:40 +0800

    Update ConsoleApplication.php

--- src/SPC/ConsoleApplication.php
+++ src/SPC/ConsoleApplication.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Command\ListCommand;
  */
 class ConsoleApplication extends Application
 {
-    public const VERSION = '2.0.0';
+    public const VERSION = '2.0-rc6';
 
     /**
      * @throws \ReflectionException
