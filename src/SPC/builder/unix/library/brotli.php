e47f3a715fafc168efdd635e6f3d77d8910f9826
crazywhalecc <jesse2061@outlook.com>
2023-09-12 00:11:41 +0800

    fix ln src and dst for brotli

--- src/SPC/builder/unix/library/brotli.php
+++ src/SPC/builder/unix/library/brotli.php
@@ -28,9 +28,9 @@ trait brotli
             ->exec('make install DESTDIR=' . BUILD_ROOT_PATH);
         $this->patchPkgconfPrefix(['libbrotlicommon.pc', 'libbrotlidec.pc', 'libbrotlienc.pc']);
         shell()->cd(BUILD_ROOT_PATH . '/lib')
-            ->exec('ln -s libbrotlicommon-static.a libbrotlicommon.a')
-            ->exec('ln -s libbrotlidec-static.a libbrotlidec.a')
-            ->exec('ln -s libbrotlienc-static.a libbrotlienc.a');
+            ->exec('ln -s libbrotlicommon.a libbrotlicommon-static.a')
+            ->exec('ln -s libbrotlidec.a libbrotlidec-static.a')
+            ->exec('ln -s libbrotlienc.a libbrotlienc-static.a');
         foreach (FileSystem::scanDirFiles(BUILD_ROOT_PATH . '/lib/', false, true) as $filename) {
             if (str_starts_with($filename, 'libbrotli') && (str_contains($filename, '.so') || str_ends_with($filename, '.dylib'))) {
                 unlink(BUILD_ROOT_PATH . '/lib/' . $filename);
