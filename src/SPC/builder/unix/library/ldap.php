1437be3a9da2f604e9bd41a83c6625ad75335fd1
DubbleClick <m@pyc.ac>
2023-09-21 12:56:52 +0200

    gmp and libsodium for ldap if enabled only enable openssl when zlib ext is also enabled (missing 'deflate' otherwise) move back from source/php-src/ext/ldap to source/ldap (fix "LICENSE not found")

--- src/SPC/builder/unix/library/ldap.php
+++ src/SPC/builder/unix/library/ldap.php
@@ -11,13 +11,18 @@ trait ldap
         shell()->cd($this->source_dir)
             ->exec(
                 $this->builder->configure_env . ' ' .
-                'LDFLAGS="-static"' .
+                'CC="musl-gcc -I' . BUILD_INCLUDE_PATH . '" ' .
+                'LDFLAGS="-static -L' . BUILD_LIB_PATH . '" ' .
+                ($this->builder->getLib('openssl') && $this->builder->getExt('zlib') ? 'LIBS="-lssl -lcrypto -lz" ' : '') .
                 ' ./configure ' .
                 '--enable-static ' .
                 '--disable-shared ' .
                 '--disable-slapd ' .
+                '--disable-slurpd ' .
                 '--without-systemd ' .
-                ($this->builder->getLib('openssl') ? '--with-tls=openssl ' : '') .
+                ($this->builder->getLib('openssl') && $this->builder->getExt('zlib') ? '--with-tls=openssl ' : '') .
+                ($this->builder->getLib('gmp') ? '--with-mp=gmp ' : '') .
+                ($this->builder->getLib('libsodium') ? '--with-argon2=libsodium ' : '') .
                 '--prefix='
             )
             ->exec('make clean')
