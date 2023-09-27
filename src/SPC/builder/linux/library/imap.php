97e2fc018a2ab87836d9918c8ecdf7f174fc7a6a
DubbleClick <m@pyc.ac>
2023-09-27 15:50:53 +0200

    use Filesystem methods, extra args

--- src/SPC/builder/linux/library/imap.php
+++ src/SPC/builder/linux/library/imap.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace SPC\builder\linux\library;
 
-/**
- * a template library class for unix
- */
 class imap extends LinuxLibraryBase
 {
     use \SPC\builder\unix\library\imap;
--- src/SPC/builder/unix/library/imap.php
+++ src/SPC/builder/unix/library/imap.php
@@ -7,6 +7,7 @@ namespace SPC\builder\unix\library;
 use SPC\exception\FileSystemException;
 use SPC\exception\RuntimeException;
 use SPC\exception\WrongUsageException;
+use SPC\store\FileSystem;
 
 trait imap
 {
@@ -17,44 +18,44 @@ trait imap
      */
     protected function build(): void
     {
-        // ext-imap is NOT thread safe, do not build with ZTS builds
-        if ($this->builder->getOption('enable-zts')) {
-            return;
-        }
+        //        ext-imap is NOT thread safe, do not build with ZTS builds
+        //        if ($this->builder->getOption('enable-zts')) {
+        //            return;
+        //        }
         if ($this->builder->getLib('openssl') !== null) {
-            $this->replace_file_contents(
+            FileSystem::replaceFileStr(
                 $this->source_dir . '/Makefile',
                 'SSLINCLUDE=/usr/include/openssl',
                 'SSLINCLUDE=' . BUILD_INCLUDE_PATH
             );
-            $this->replace_file_contents(
+            FileSystem::replaceFileStr(
                 $this->source_dir . '/Makefile',
                 'SSLLIB=/usr/lib',
                 'SSLLIB=' . BUILD_LIB_PATH
             );
-            $this->replace_file_contents(
+            FileSystem::replaceFileStr(
                 $this->source_dir . '/Makefile',
                 '"-DMAC_OSX_KLUDGE=1',
                 ''
             );
-            $this->replace_file_contents(
+            FileSystem::replaceFileStr(
                 $this->source_dir . '/src/osdep/unix/Makefile',
-                '-lcrypto -lz',
+                '-lcrypto -lz -lpam',
                 '-lcrypto'
             );
-            $this->replace_file_contents(
+            FileSystem::replaceFileStr(
                 $this->source_dir . '/src/osdep/unix/Makefile',
                 '-lcrypto',
-                '-lcrypto -lz'
+                '-lcrypto -lz -lpam'
             );
-            $this->replace_file_contents(
+            FileSystem::replaceFileStr(
                 $this->source_dir . '/src/osdep/unix/ssl_unix.c',
                 "#include <x509v3.h>\n#include <ssl.h>",
                 "#include <ssl.h>\n#include <x509v3.h>"
             );
             // https://salsa.debian.org/holmgren/uw-imap/raw/master/debian/patches/2014_openssl1.1.1_sni.patch
             // this was not applied in the github source yet
-            $this->replace_file_contents(
+            FileSystem::replaceFileStr(
                 $this->source_dir . '/src/osdep/unix/ssl_unix.c',
                 <<<'EOL'
 if (!(stream->con = (SSL *) SSL_new (stream->context)))
@@ -80,24 +81,14 @@ EOL
         shell()->cd($this->source_dir)
             ->exec('touch ip6')
             ->exec(
-                "{$this->builder->configure_env} make {$platform}" .
-                ($this->builder->getLib('openssl') === null ? ' SSLTYPE=none' : '')
+                "{$this->builder->configure_env} make {$platform} " .
+                'EXTRACFLAGS="-fPIC" ' .
+                ($this->builder->getLib('openssl') === null ? ' SSLTYPE=none' : 'SPECIALAUTHENTICATORS=ssl SSLTYPE=unix')
             );
         shell()
             ->exec("cp -rf {$this->source_dir}/c-client/c-client.a " . BUILD_LIB_PATH . '/libc-client.a')
-            ->exec("cp -rf {$this->source_dir}/c-client/*.h " . BUILD_INCLUDE_PATH)
-            ->exec("cp -rf {$this->source_dir}/src/osdep/unix/*.h " . BUILD_INCLUDE_PATH);
-    }
-
-    private function replace_file_contents(string|array $filepath, string $old, string $new): void
-    {
-        if (is_string($filepath)) {
-            $filepath = [$filepath];
-        }
-        foreach ($filepath as $path) {
-            $str = file_get_contents($path);
-            $str = str_replace($old, $new, $str);
-            file_put_contents($path, $str);
-        }
+            ->exec("cp -rf {$this->source_dir}/c-client/*.c " . BUILD_LIB_PATH . '/')
+            ->exec("cp -rf {$this->source_dir}/c-client/*.h " . BUILD_INCLUDE_PATH . '/')
+            ->exec("cp -rf {$this->source_dir}/src/osdep/unix/*.h " . BUILD_INCLUDE_PATH . '/');
     }
 }
