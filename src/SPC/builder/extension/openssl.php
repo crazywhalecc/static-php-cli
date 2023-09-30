5228142451b9dd3c3c32573ec70d7551bde7aced
Joseph Bielawski <stloyd@gmail.com>
2023-09-30 08:56:37 +0200

    Throw proper exception when PHP source is not available

--- src/SPC/builder/extension/openssl.php
+++ src/SPC/builder/extension/openssl.php
@@ -6,7 +6,6 @@ namespace SPC\builder\extension;
 
 use SPC\builder\Extension;
 use SPC\util\CustomExt;
-use SPC\util\Util;
 
 #[CustomExt('openssl')]
 class openssl extends Extension
@@ -14,12 +13,13 @@ class openssl extends Extension
     public function patchBeforeMake(): bool
     {
         // patch openssl3 with php8.0 bug
-        if (file_exists(SOURCE_PATH . '/openssl/VERSION.dat') && Util::getPHPVersionID() < 80100) {
+        if (file_exists(SOURCE_PATH . '/openssl/VERSION.dat') && $this->builder->getPHPVersionID() < 80100) {
             $openssl_c = file_get_contents(SOURCE_PATH . '/php-src/ext/openssl/openssl.c');
             $openssl_c = preg_replace('/REGISTER_LONG_CONSTANT\s*\(\s*"OPENSSL_SSLV23_PADDING"\s*.+;/', '', $openssl_c);
             file_put_contents(SOURCE_PATH . '/php-src/ext/openssl/openssl.c', $openssl_c);
             return true;
         }
+
         return false;
     }
 }
