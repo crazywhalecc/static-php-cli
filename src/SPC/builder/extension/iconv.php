394426f375ec0823aa349d172a479db92fa33b67
DubbleClick <m@pyc.ac>
2023-10-11 19:07:02 +0200

    full libiconv path instead of -liconv

--- src/SPC/builder/extension/iconv.php
+++ src/SPC/builder/extension/iconv.php
@@ -14,8 +14,8 @@ class iconv extends Extension
     {
         // macOS need to link iconv dynamically, we add it to extra-libs
         $extra_libs = $this->builder->getOption('extra-libs', '');
-        if (!str_contains($extra_libs, '-liconv')) {
-            $extra_libs .= ' -liconv';
+        if (!str_contains($extra_libs, 'iconv')) {
+            $extra_libs .= ' ' . BUILD_LIB_PATH . '/libiconv.a';
         }
         $this->builder->setOption('extra-libs', $extra_libs);
         return true;
