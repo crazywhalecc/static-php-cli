7a010ca822cb87b798532457aeeb4e19eddd9c46
DubbleClick <m@pyc.ac>
2023-10-13 16:09:29 +0200

    icu 73.2 flags

--- src/SPC/builder/linux/library/icu.php
+++ src/SPC/builder/linux/library/icu.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace SPC\builder\linux\library;
 
+use SPC\builder\linux\SystemUtil;
+
 class icu extends LinuxLibraryBase
 {
     public const NAME = 'icu';
@@ -13,8 +15,8 @@ class icu extends LinuxLibraryBase
         $cppflags = 'CPPFLAGS="-DU_CHARSET_IS_UTF8=1  -DU_USING_ICU_NAMESPACE=1  -DU_STATIC_IMPLEMENTATION=1"';
         $cxxflags = 'CXXFLAGS="-std=c++11"';
         $ldflags = 'LDFLAGS="-static"';
-        $libs = file_exists('/usr/local/musl/lib/libstdc++.a') ? 'LIBS="/usr/local/musl/lib/libstdc++.a"' : 'LIBS="-lstdc++"';
-        shell()->cd($this->source_dir . '/icu4c/source')
+        $libs = SystemUtil::getOSRelease()['dist'] === 'alpine' ? '' : 'LIBS="/usr/local/musl/lib/libstdc++.a"';
+        shell()->cd($this->source_dir . '/source')
             ->exec(
                 "{$this->builder->configure_env} {$cppflags} {$cxxflags} {$ldflags} {$libs} " .
                 './runConfigureICU Linux ' .
