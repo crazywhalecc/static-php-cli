9c0a6bd5cd73e87353279762aa6080462314f591
DubbleClick <m@pyc.ac>
2023-10-13 15:30:53 +0200

    make php-cs-fixer happy

--- src/SPC/store/FileSystem.php
+++ src/SPC/store/FileSystem.php
@@ -255,14 +255,13 @@ class FileSystem
     /**
      * 递归或非递归扫描目录，可返回相对目录的文件列表或绝对目录的文件列表
      *
-     * @param  string      $dir         目录
-     * @param  bool        $recursive   是否递归扫描子目录
-     * @param  bool|string $relative    是否返回相对目录，如果为true则返回相对目录，如果为false则返回绝对目录
-     * @param  bool        $include_dir 非递归模式下，是否包含目录
-     * @return array|false
+     * @param string      $dir         目录
+     * @param bool        $recursive   是否递归扫描子目录
+     * @param bool|string $relative    是否返回相对目录，如果为true则返回相对目录，如果为false则返回绝对目录
+     * @param bool        $include_dir 非递归模式下，是否包含目录
      * @since 2.5
      */
-    public static function scanDirFiles(string $dir, bool $recursive = true, bool|string $relative = false, bool $include_dir = false): bool|array
+    public static function scanDirFiles(string $dir, bool $recursive = true, bool|string $relative = false, bool $include_dir = false): array|false
     {
         $dir = self::convertPath($dir);
         // 不是目录不扫，直接 false 处理
@@ -402,7 +401,7 @@ class FileSystem
      * @param  mixed               ...$args Arguments passed to file_put_contents
      * @throws FileSystemException
      */
-    public static function writeFile(string $path, mixed $content, ...$args): bool|string|int
+    public static function writeFile(string $path, mixed $content, ...$args): bool|int|string
     {
         $dir = pathinfo($path, PATHINFO_DIRNAME);
         if (!is_dir($dir) && !mkdir($dir, 0755, true)) {
