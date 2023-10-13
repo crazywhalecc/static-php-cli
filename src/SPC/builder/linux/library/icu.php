de18ed30389de74a4ec370c3baad318874ea7e73
Marc Henderkes <m.henderkes@pc-college.de>
2023-10-13 10:30:35 +0200

    update icu

--- src/SPC/builder/linux/library/icu.php
+++ src/SPC/builder/linux/library/icu.php
@@ -4,22 +4,18 @@ declare(strict_types=1);
 
 namespace SPC\builder\linux\library;
 
-use SPC\builder\linux\SystemUtil;
-
 class icu extends LinuxLibraryBase
 {
     public const NAME = 'icu';
 
     protected function build(): void
     {
-        $root = BUILD_ROOT_PATH;
-        $is_alpine = SystemUtil::getOSRelease()['dist'] === 'alpine';
-        $arch = arch2gnu(php_uname('m')) === 'x86_64' ? 'x86_64-linux-musl/' : 'aarch64-linux-musl/';
-        $ld_library_path = $is_alpine ? '' : "LD_LIBRARY_PATH=/usr/local/musl/{$arch}/lib ";
         $cppflags = 'CPPFLAGS="-DU_CHARSET_IS_UTF8=1  -DU_USING_ICU_NAMESPACE=1  -DU_STATIC_IMPLEMENTATION=1"';
-        shell()->cd($this->source_dir . '/source')
+        $ldflags = 'LDFLAGS="-static" LIBS="/usr/local/musl/lib/libstdc++.a"';
+        shell()->cd($this->source_dir . '/icu4c/source')
             ->exec(
-                "{$this->builder->configure_env} {$cppflags} ./runConfigureICU Linux " .
+                "{$this->builder->configure_env} {$cppflags} {$ldflags} " .
+                './runConfigureICU Linux ' .
                 '--enable-static ' .
                 '--disable-shared ' .
                 '--with-data-packaging=static ' .
@@ -30,10 +26,10 @@ class icu extends LinuxLibraryBase
                 '--enable-tools=yes ' .
                 '--enable-tests=no ' .
                 '--enable-samples=no ' .
-                "--prefix={$root}"
+                '--prefix=' . BUILD_ROOT_PATH
             )
             ->exec('make clean')
-            ->exec("{$ld_library_path}make -j{$this->builder->concurrency}")
-            ->exec("{$ld_library_path}make install");
+            ->exec("make -j{$this->builder->concurrency}")
+            ->exec('make install');
     }
 }
