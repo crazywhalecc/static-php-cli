78cd717c5c156216801cbbcf667be7c6562cb1e1
crazywhalecc <jesse2061@outlook.com>
2023-10-17 18:34:23 +0800

    move option `rebuild` add position

--- src/SPC/command/BuildCommand.php
+++ src/SPC/command/BuildCommand.php
@@ -30,5 +30,6 @@ abstract class BuildCommand extends BaseCommand
 
         $this->addOption('with-clean', null, null, 'fresh build, remove `source` dir before `make`');
         $this->addOption('bloat', null, null, 'add all libraries into binary');
+        $this->addOption('rebuild', 'r', null, 'Delete old build and rebuild');
     }
 }
