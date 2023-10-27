e84a246dd22305afa33797a4114e860f194f5d78
DubbleClick <m@pyc.ac>
2023-10-27 17:33:43 +0200

    add back required -lgomp for imagick extension

--- src/SPC/builder/extension/imagick.php
+++ src/SPC/builder/extension/imagick.php
@@ -14,6 +14,7 @@ class imagick extends Extension
     {
         // imagick may call omp_pause_all which requires -lgomp
         $extra_libs = $this->builder->getOption('extra-libs', '');
+        $extra_libs .= ' -lgomp ';
         $this->builder->setOption('extra-libs', $extra_libs);
         return true;
     }
