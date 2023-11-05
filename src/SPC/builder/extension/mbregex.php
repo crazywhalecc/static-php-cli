8154ea23c77dd9c6cc57205f1c06bcd8b46ff479
crazywhalecc <jesse2061@outlook.com>
2023-11-05 17:48:20 +0800

    overwrite mbregex cli check

--- src/SPC/builder/extension/mbregex.php
+++ src/SPC/builder/extension/mbregex.php
@@ -5,18 +5,25 @@ declare(strict_types=1);
 namespace SPC\builder\extension;
 
 use SPC\builder\Extension;
+use SPC\exception\RuntimeException;
 use SPC\util\CustomExt;
 
 #[CustomExt('mbregex')]
 class mbregex extends Extension
 {
-    public function getDistName(): string
-    {
-        return 'mbstring';
-    }
-
     public function getConfigureArg(): string
     {
         return '';
+    }
+
+    /**
+     * mbregex is not an extension, we need to overwrite the default check.
+     */
+    public function runCliCheck(): void
+    {
+        [$ret] = shell()->execWithResult(BUILD_ROOT_PATH . '/bin/php --ri "mbstring" | grep regex', false);
+        if ($ret !== 0) {
+            throw new RuntimeException('extension ' . $this->getName() . ' failed compile check: compiled php-cli mbstring extension does not contain regex !');
+        }
     }
 }
