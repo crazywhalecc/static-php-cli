2e637524fce5dca2bee1893671d94a9c49a765e8
crazywhalecc <jesse2061@outlook.com>
2023-11-19 13:38:55 +0800

    Add test build things

---
+++ src/globals/test-extensions.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+# If you want to test new extensions here, just modify it.
+$extensions = 'bcmath,bz2,calendar,ctype,curl,dom,exif,fileinfo,filter,ftp,gd,gmp,iconv,xml,mbstring,mbregex,mysqlnd,openssl,pcntl,pdo,pdo_mysql,pdo_sqlite,phar,posix,redis,session,simplexml,soap,sockets,sqlite3,tokenizer,xmlwriter,xmlreader,zlib,zip';
+
+echo $extensions;
