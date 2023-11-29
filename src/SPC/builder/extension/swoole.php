930849758c74fb7e57c851bbf2eb133275cb91f4
jingjingxyk <zonghengbaihe521@qq.com>
2023-11-29 22:28:20 +0800

    swoole.php and LinuxBuilder.php revert

--- src/SPC/builder/extension/swoole.php
+++ src/SPC/builder/extension/swoole.php
@@ -12,24 +12,13 @@ class swoole extends Extension
 {
     public function getUnixConfigureArg(): string
     {
-        $arg = ' --enable-swoole ';
-
-        $options = '';
-        if ($this->builder->getLib('postgresql')) {
-            if (!$this->builder->getExt('pdo_pgsql')) {
-                $options .= ' --enable-swoole-pgsql ';
-            }
-        } else {
-            $options .= ' --disable-swoole-pgsql ';
-        }
-        $arg .= $options;
-        $arg .= $this->builder->getLib('openssl') ? ' --enable-openssl ' : ' --disable-openssl --without-openssl ';
-        $arg .= $this->builder->getLib(
-            'brotli'
-        ) ? (' --enable-brotli --with-brotli-dir=' . BUILD_ROOT_PATH) : ' --disable-brotli ';
+        $arg = '--enable-swoole';
+        // pgsql hook is buggy for static php
+        $arg .= ' --disable-swoole-pgsql';
+        $arg .= $this->builder->getLib('openssl') ? ' --enable-openssl' : ' --disable-openssl --without-openssl';
+        $arg .= $this->builder->getLib('brotli') ? (' --enable-brotli --with-brotli-dir=' . BUILD_ROOT_PATH) : ' --disable-brotli';
         // swoole curl hook is buggy for php 8.0
-        $arg .= $this->builder->getExt('curl') && $this->builder->getPHPVersionID(
-        ) >= 80100 ? ' --enable-swoole-curl' : ' --disable-swoole-curl ';
+        $arg .= $this->builder->getExt('curl') && $this->builder->getPHPVersionID() >= 80100 ? ' --enable-swoole-curl' : ' --disable-swoole-curl';
         return $arg;
     }
 }
