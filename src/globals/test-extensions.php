400007c206a5e085410b9dee9a0a0f141c7483fd
jingjingxyk <zonghengbaihe521@qq.com>
2023-11-29 15:56:52 +0800

    test change test-extensions

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 # If you want to test new extensions here, just modify it.
-$extensions = 'pgsql,openssl,posix,pcntl,phar,zip,pdo_pgsql';
-
+$extensions =
+'apcu,bcmath,calendar,ctype,curl,dba,dom,exif,filter,fileinfo,gd,iconv,intl,mbstring,mbregex,mysqli,mysqlnd,openssl,opcache,pcntl,pdo,pdo_mysql,pdo_sqlite,pdo_pgsql,pgsql,phar,posix,readline,redis,session,simplexml,sockets,sodium,sqlite3,tokenizer,xml,xmlreader,xmlwriter,xsl,zip,zlib,bz2,event,gmp,imagick,imap,ldap,protobuf,shmop,snappy,soap,swoole,sysvmsg,sysvsem,sysvshm,tidy,zstd';
 echo $extensions;
