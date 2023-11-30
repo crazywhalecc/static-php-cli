3185d98b1a9ba383cf97b7e8b0e6f6656dd683ae
jingjingxyk <zonghengbaihe521@qq.com>
2023-11-30 13:00:04 +0800

    test libpq build

--- src/SPC/builder/unix/library/postgresql.php
+++ src/SPC/builder/unix/library/postgresql.php
@@ -78,7 +78,7 @@ trait postgresql
                 '--with-readline ' .
                 '--with-libxml ' .
                 ($this->builder->getLib('icu') ? '--with-icu ' : '--without-icu ') .
-                ($this->builder->getLib('ldap') && 0 ? '--with-ldap ' : '--without-ldap ') .
+                (($this->builder->getLib('ldap') && 0) ? '--with-ldap ' : '--without-ldap ') .
                 ($this->builder->getLib('libxslt') ? '--with-libxslt ' : '--without-libxslt ') .
                 ($this->builder->getLib('zstd') ? '--with-zstd ' : '--without-zstd ') .
                 '--without-lz4 ' .
