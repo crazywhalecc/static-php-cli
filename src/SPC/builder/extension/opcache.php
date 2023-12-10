16be451837a32d627fb92c10afec56eec972f13a
crazywhalecc <jesse2061@outlook.com>
2023-12-10 18:27:52 +0800

    opcache limit php version

--- src/SPC/builder/extension/opcache.php
+++ src/SPC/builder/extension/opcache.php
@@ -5,11 +5,25 @@ declare(strict_types=1);
 namespace SPC\builder\extension;
 
 use SPC\builder\Extension;
+use SPC\exception\RuntimeException;
+use SPC\exception\WrongUsageException;
 use SPC\util\CustomExt;
 
 #[CustomExt('opcache')]
 class opcache extends Extension
 {
+    /**
+     * @throws WrongUsageException
+     * @throws RuntimeException
+     */
+    public function getUnixConfigureArg(): string
+    {
+        if ($this->builder->getPHPVersionID() < 80000) {
+            throw new WrongUsageException('Statically compiled PHP with Zend Opcache only available for PHP >= 8.0 !');
+        }
+        return '--enable-opcache';
+    }
+
     public function getDistName(): string
     {
         return 'Zend Opcache';
