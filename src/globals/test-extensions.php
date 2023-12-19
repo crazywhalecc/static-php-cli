248918e4580fd3b52b0a8459daca53b1014f5232
crazywhalecc <jesse2061@outlook.com>
2023-12-19 20:04:13 +0800

    remove ldap test for known reason

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 $extensions = 'password-argon2,apcu,bcmath,bz2,calendar,ctype,curl,dba,dom,event,exif,fileinfo,filter,ftp,gd,gmp,iconv,imagick,imap,intl,mbregex,mbstring,mysqli,mysqlnd,opcache,openssl,pcntl,pdo,pdo_mysql,pdo_pgsql,pdo_sqlite,pgsql,phar,posix,protobuf,readline,redis,session,shmop,simplexml,soap,sockets,sqlite3,swoole,sysvmsg,sysvsem,sysvshm,tokenizer,xml,xmlreader,xmlwriter,xsl,zip,zlib';
 
 if (PHP_OS_FAMILY === 'Darwin') {
-    $extensions .= ',ldap,sodium';
+    $extensions .= ',sodium';
 }
 
 echo $extensions;
