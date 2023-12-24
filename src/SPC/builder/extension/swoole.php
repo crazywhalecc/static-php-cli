8fa9ca91eca13d85504bbc5f0119881df212afa9
jingjingxyk <zonghengbaihe521@qq.com>
2023-12-24 11:51:11 +0800

    improve swoole static build config

--- src/SPC/builder/extension/swoole.php
+++ src/SPC/builder/extension/swoole.php
@@ -13,7 +13,7 @@ class swoole extends Extension
     public function getUnixConfigureArg(): string
     {
         $arg = ' --enable-swoole --enable-sockets --enable-mysqlnd  --enable-cares ';
-        # $arg .= ' --with-brotli-dir=' . BUILD_ROOT_PATH;
+        $arg .= ' --with-brotli-dir=' . BUILD_ROOT_PATH;
         $arg .= ' --with-nghttp2-dir=' . BUILD_ROOT_PATH;
 
         $arg .= $this->builder->getExt('pgsql') ? ' --enable-swoole-pgsql' : ' ';
