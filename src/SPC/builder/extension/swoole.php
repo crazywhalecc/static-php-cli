8590a5c0b7ee0ec40b4b36415e11e6ea85a22fe8
jingjingxyk <zonghengbaihe521@qq.com>
2023-12-25 00:12:11 +0800

    test

--- src/SPC/builder/extension/swoole.php
+++ src/SPC/builder/extension/swoole.php
@@ -13,10 +13,19 @@ class swoole extends Extension
     public function getUnixConfigureArg(): string
     {
         $arg = ' --enable-swoole --enable-sockets --enable-mysqlnd  --enable-cares ';
+        $arg .= ' --enable-swoole-coro-time --enable-thread-context';
         $arg .= ' --with-brotli-dir=' . BUILD_ROOT_PATH;
         $arg .= ' --with-nghttp2-dir=' . BUILD_ROOT_PATH;
 
-        $arg .= $this->builder->getLib('postgresql') ? ' --enable-swoole-pgsql' : ' ';
+        $arg .= $this->builder->getLib('postgresql') ? ' --enable-swoole-pgsql ' : ' ';
+
+        // enable this feature , need remove pdo_sqlite
+        // more info : https://wenda.swoole.com/detail/109023
+        // $arg .= $this->builder->getLib('sqlite') ? ' --enable-swoole-sqlite ' : ' ';
+
+        // enable this feature , need stop pdo_*
+        // $arg .= $this->builder->getLib('unixodbc') ? ' --with-swoole-odbc=unixODBC,'  : ' ';
+
         // swoole curl hook is buggy for php 8.0
         $arg .= $this->builder->getPHPVersionID() >= 80100 ? ' --enable-swoole-curl' : ' --disable-swoole-curl';
 
