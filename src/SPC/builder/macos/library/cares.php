b277d1801798ca528ad712b2067416c32af088c0
jingjingxyk <zonghengbaihe521@qq.com>
2023-12-24 12:26:18 +0800

    update cares build   config

--- src/SPC/builder/macos/library/cares.php
+++ src/SPC/builder/macos/library/cares.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace SPC\builder\macos\library;
 
-use SPC\builder\linux\library\LinuxLibraryBase;
-
-class cares extends LinuxLibraryBase
+class cares extends MacOSLibraryBase
 {
     use \SPC\builder\unix\library\cares;
 
