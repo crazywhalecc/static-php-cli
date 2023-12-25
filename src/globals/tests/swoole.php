122d7cbab220d3a2faa243e16979fab60708c83f
jingjingxyk <zonghengbaihe521@qq.com>
2023-12-26 00:34:05 +0800

    update swoole extension test case

--- src/globals/tests/swoole.php
+++ src/globals/tests/swoole.php
@@ -2,64 +2,17 @@
 
 declare(strict_types=1);
 
-use Swoole\Coroutine;
-use Swoole\Coroutine\Http2\Client;
-use Swoole\Coroutine\WaitGroup;
-use Swoole\Http2\Request;
-
 assert(function_exists('swoole_cpu_num'));
+assert(function_exists('swoole_string'));
+assert(class_exists('Swoole\Coroutine'));
+assert(class_exists('Swoole\Coroutine\Http2\Client'));
 assert(class_exists('Swoole\Coroutine\Redis'));
+assert(class_exists('Swoole\Coroutine\WaitGroup'));
+assert(class_exists('Swoole\Http2\Request'));
+assert(constant('SWOOLE_VERSION'));
 
-co::set([
-    'trace_flags' => SWOOLE_TRACE_HTTP2,
-    'log_level' => 0,
-]);
-
-Swoole\Coroutine\run(function () {
-    $domain = 'api.github.com';
-    $cli = new Client($domain, 443, true);
-    $cli->set([
-        'timeout' => -1,
-        'ssl_host_name' => $domain,
-    ]);
-    $cli->connect();
-    $req = new Request();
-    $req->method = 'GET';
-    $req->path = '/repos/webtorrent/webtorrent';
-    $req->headers = [
-        'host' => $domain,
-        'user-agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        'accept' => 'application/json',
-        'accept-encoding' => 'gzip',
-    ];
-
-    $cli->send($req);
-    $response = $cli->recv();
-    // var_dump(json_decode($response->data));
-    assert($response->statusCode == 200);
-
-    Coroutine::create(function () {
-        $content = file_get_contents('https://www.xinhuanet.com/');
-        assert(strlen($content) > 0);
-    });
-
-    Coroutine::create(function () {
-        Coroutine::sleep(1);
-    });
-
-    $wg = new WaitGroup();
-    $ret = [];
-    for ($i = 0; $i < 10; ++$i) {
-        $wg->add();
-        go(function () use ($wg, $i, &$ret) {
-            co::sleep(0.1);
-            $ret[$i] = $i;
-            $wg->done();
-        });
-    }
-    $wg->wait();
-    assert(count($ret) == 10);
-
-    $ip = Swoole\Coroutine\System::gethostbyname('www.baidu.com', AF_INET, 0.5);
-    echo 'baidu.com dns: ' . $ip . PHP_EOL;
-});
+$command = 'ls -1 ' . __DIR__;
+$list = swoole_string(shell_exec($command))->trim()->lower()->split(PHP_EOL)
+    ->remove('calendar.php')
+    ->remove('zlib.php')->toArray();
+assert(in_array('swoole.phpt', $list));
