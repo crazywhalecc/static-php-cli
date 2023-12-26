b6546bbc5a8c94797c79df02b6c3c8793ea59b11
crazywhalecc <jesse2061@outlook.com>
2023-12-27 01:29:49 +0800

    re-adjust swoole args

--- src/SPC/builder/extension/swoole.php
+++ src/SPC/builder/extension/swoole.php
@@ -12,12 +12,23 @@ class swoole extends Extension
 {
     public function getUnixConfigureArg(): string
     {
-        $arg = ' --enable-swoole --enable-sockets --enable-mysqlnd  --enable-cares ';
-        $arg .= ' --enable-swoole-coro-time --enable-thread-context';
-        $arg .= ' --with-brotli-dir=' . BUILD_ROOT_PATH;
-        $arg .= ' --with-nghttp2-dir=' . BUILD_ROOT_PATH;
+        // enable swoole
+        $arg = '--enable-swoole';
 
-        $arg .= $this->builder->getLib('postgresql') ? ' --enable-swoole-pgsql ' : ' ';
+        // commonly-used feature: coroutine-time, thread-context
+        $arg .= ' --enable-swoole-coro-time --enable-thread-context';
+
+        // required feature: curl, openssl (but curl hook is buggy for php 8.0)
+        $arg .= $this->builder->getPHPVersionID() >= 80100 ? ' --enable-swoole-curl' : ' --disable-swoole-curl';
+        $arg .= ' --enable-openssl';
+
+        // additional feature: c-ares, brotli, nghttp2 (can be disabled, but we enable it by default in config)
+        $arg .= $this->builder->getLib('libcares') ? ' --enable-cares' : '';
+        $arg .= $this->builder->getLib('brotli') ? (' --with-brotli-dir=' . BUILD_ROOT_PATH) : '';
+        $arg .= $this->builder->getLib('nghttp2') ? (' --with-nghttp2-dir=' . BUILD_ROOT_PATH) : '';
+
+        // additional feature: pgsql
+        $arg .= $this->builder->getExt('pgsql') ? ' --enable-swoole-pgsql ' : '';
 
         // enable this feature , need remove pdo_sqlite
         // more info : https://wenda.swoole.com/detail/109023
@@ -25,10 +36,6 @@ class swoole extends Extension
 
         // enable this feature , need stop pdo_*
         // $arg .= $this->builder->getLib('unixodbc') ? ' --with-swoole-odbc=unixODBC,'  : ' ';
-
-        // swoole curl hook is buggy for php 8.0
-        $arg .= $this->builder->getPHPVersionID() >= 80100 ? ' --enable-swoole-curl' : ' --disable-swoole-curl';
-
         return $arg;
     }
 }
