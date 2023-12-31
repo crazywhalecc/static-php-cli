a96d29c2876e6268569a5fbcf5da05de1e6b49ba
crazywhalecc <jesse2061@outlook.com>
2023-12-31 15:39:24 +0800

    use addon mode to swoole database hook

--- src/SPC/builder/extension/swoole.php
+++ src/SPC/builder/extension/swoole.php
@@ -22,17 +22,18 @@ class swoole extends Extension
         $arg .= $this->builder->getPHPVersionID() >= 80100 ? ' --enable-swoole-curl' : ' --disable-swoole-curl';
         $arg .= ' --enable-openssl';
 
-        // additional feature: c-ares, brotli, nghttp2 (can be disabled, but we enable it by default in config)
+        // additional feature: c-ares, brotli, nghttp2 (can be disabled, but we enable it by default in config to support full network feature)
         $arg .= $this->builder->getLib('libcares') ? ' --enable-cares' : '';
         $arg .= $this->builder->getLib('brotli') ? (' --with-brotli-dir=' . BUILD_ROOT_PATH) : '';
         $arg .= $this->builder->getLib('nghttp2') ? (' --with-nghttp2-dir=' . BUILD_ROOT_PATH) : '';
 
-        // additional feature: pgsql
-        $arg .= $this->builder->getExt('pgsql') ? ' --enable-swoole-pgsql ' : '';
+        // additional feature: swoole-pgsql, it should depend on lib [postgresql], but it will lack of CFLAGS etc.
+        // so this is a tricky way (enable ext [pgsql,pdo] to add postgresql hook and pdo_pgsql support)
+        $arg .= $this->builder->getExt('swoole-hook-pgsql') ? '' : ' --disable-swoole-pgsql';
 
         // enable this feature , need remove pdo_sqlite
         // more info : https://wenda.swoole.com/detail/109023
-        // $arg .= $this->builder->getLib('sqlite') ? ' --enable-swoole-sqlite ' : ' ';
+        $arg .= $this->builder->getExt('swoole-hook-sqlite') ? '' : ' --disable-swoole-sqlite';
 
         // enable this feature , need stop pdo_*
         // $arg .= $this->builder->getLib('unixodbc') ? ' --with-swoole-odbc=unixODBC,'  : ' ';
