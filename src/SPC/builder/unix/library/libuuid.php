cbd75eb796bd8057c533639cc3a1ced3ebf7213a
crazywhalecc <jesse2061@outlook.com>
2024-03-01 21:12:07 +0800

    test

--- src/SPC/builder/unix/library/libuuid.php
+++ src/SPC/builder/unix/library/libuuid.php
@@ -30,6 +30,7 @@ trait libuuid
                 '--enable-static --disable-shared ' .
                 '--prefix='
             )
+            ->exec('cat Makefile')
             ->exec('make clean')
             ->exec("make -j{$this->builder->concurrency}")
             ->exec('make install DESTDIR=' . BUILD_ROOT_PATH);
