f5d1df54076723a4ed15f98f84e8f873ffab7a29
Jerry Ma <jesse2061@outlook.com>
2024-03-01 20:10:48 +0800

    add uuid/libuuid for linux and macos (#363)

--- src/SPC/ConsoleApplication.php
+++ src/SPC/ConsoleApplication.php
@@ -25,7 +25,7 @@ use Symfony\Component\Console\Command\ListCommand;
  */
 final class ConsoleApplication extends Application
 {
-    public const VERSION = '2.1.2';
+    public const VERSION = '2.1.3';
 
     public function __construct()
     {
