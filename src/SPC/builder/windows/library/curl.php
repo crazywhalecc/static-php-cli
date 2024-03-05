f2796e9ee6f6fd41889c3f65423724cd523cd9c8
crazywhalecc <jesse2061@outlook.com>
2024-03-05 15:54:04 +0800

    fix curl on windows build needs nghttp2.dll bug

--- src/SPC/builder/windows/library/curl.php
+++ src/SPC/builder/windows/library/curl.php
@@ -12,29 +12,12 @@ class curl extends WindowsLibraryBase
 
     protected function build(): void
     {
-        // reset cmake
-        FileSystem::resetDir($this->source_dir . '\build');
-
-        // start build
-        cmd()->cd($this->source_dir)
+        cmd()->cd($this->source_dir . '\winbuild')
             ->execWithWrapper(
-                $this->builder->makeSimpleWrapper('cmake'),
-                '-B build ' .
-                '-A x64 ' .
-                "-DCMAKE_TOOLCHAIN_FILE={$this->builder->cmake_toolchain_file} " .
-                '-DCMAKE_BUILD_TYPE=Release ' .
-                '-DBUILD_SHARED_LIBS=OFF ' .
-                '-DBUILD_STATIC_LIBS=ON ' .
-                '-DBUILD_CURL_EXE=OFF ' .
-                '-DUSE_ZLIB=ON ' .
-                '-DCURL_USE_OPENSSL=ON ' .
-                '-DCURL_USE_LIBLSSH2=ON ' .
-                '-DUSE_NGHTTP2=ON ' . // php-src with curl needs nghttp2
-                '-DCMAKE_INSTALL_PREFIX=' . BUILD_ROOT_PATH . ' '
-            )
-            ->execWithWrapper(
-                $this->builder->makeSimpleWrapper('cmake'),
-                "--build build --config Release --target install -j{$this->builder->concurrency}"
+                $this->builder->makeSimpleWrapper('nmake'),
+                '/f Makefile.vc WITH_DEVEL=' . BUILD_ROOT_PATH . ' ' .
+                'WITH_PREFIX=' . BUILD_ROOT_PATH . ' ' .
+                'mode=static RTLIBCFG=static WITH_SSL=static WITH_NGHTTP2=static WITH_SSH2=static ENABLE_IPV6=yes WITH_ZLIB=static MACHINE=x64 DEBUG=no'
             );
     }
 }
--- src/SPC/builder/windows/library/nghttp2.php
+++ src/SPC/builder/windows/library/nghttp2.php
@@ -23,8 +23,8 @@ class nghttp2 extends WindowsLibraryBase
                 '-A x64 ' .
                 "-DCMAKE_TOOLCHAIN_FILE={$this->builder->cmake_toolchain_file} " .
                 '-DCMAKE_BUILD_TYPE=Release ' .
-                '-DBUILD_SHARED_LIBS=OFF ' .
-                '-DBUILD_STATIC_LIBS=ON ' .
+                '-DENABLE_SHARED_LIB=OFF ' .
+                '-DENABLE_STATIC_LIB=ON ' .
                 '-DENABLE_STATIC_CRT=ON ' .
                 '-DENABLE_LIB_ONLY=ON ' .
                 '-DCMAKE_INSTALL_PREFIX=' . BUILD_ROOT_PATH . ' '
