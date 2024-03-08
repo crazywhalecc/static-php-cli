d202de3f502f9180034740ca52244951acf34922
crazywhalecc <jesse2061@outlook.com>
2024-03-08 14:10:12 +0800

    fix libxml2 build on RHEL/CentOS bug

--- src/SPC/builder/macos/library/libxml2.php
+++ src/SPC/builder/macos/library/libxml2.php
@@ -29,6 +29,7 @@ class libxml2 extends MacOSLibraryBase
                 // '--debug-find ' .
                 '-DCMAKE_BUILD_TYPE=Release ' .
                 '-DCMAKE_INSTALL_PREFIX=' . BUILD_ROOT_PATH . ' ' .
+                '-DCMAKE_INSTALL_LIBDIR=' . BUILD_LIB_PATH . ' ' .
                 "-DCMAKE_TOOLCHAIN_FILE={$this->builder->cmake_toolchain_file} " .
                 '-DBUILD_SHARED_LIBS=OFF ' .
                 '-DLIBXML2_WITH_ICONV=ON ' .
