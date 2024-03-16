19307910e86ee222b5c666d994f94b2180e24d99
crazywhalecc <jesse2061@outlook.com>
2024-03-16 21:45:15 +0800

    test linux compatibility for libargon2 and libsodium

--- src/SPC/builder/linux/library/libargon2.php
+++ src/SPC/builder/linux/library/libargon2.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace SPC\builder\linux\library;
 
-use SPC\exception\WrongUsageException;
 use SPC\store\FileSystem;
 
 class libargon2 extends LinuxLibraryBase
@@ -15,10 +14,6 @@ class libargon2 extends LinuxLibraryBase
 
     public function patchBeforeBuild(): bool
     {
-        // detect libsodium (The libargon2 conflicts with the libsodium library.)
-        if ($this->builder->getLib('libsodium') !== null) {
-            throw new WrongUsageException('libargon2 (required by password-argon2) conflicts with the libsodium library !');
-        }
         FileSystem::replaceFileStr($this->source_dir . '/Makefile', 'LIBRARY_REL ?= lib/x86_64-linux-gnu', 'LIBRARY_REL ?= lib');
         return true;
     }
