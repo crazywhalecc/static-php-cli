f6c42e0fbf21b49a11cbf82539b83e7de51e2444
crazywhalecc <jesse2061@outlook.com>
2024-04-12 12:44:26 +0800

    add qdbm for dba

---
+++ src/SPC/builder/extension/dba.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SPC\builder\extension;
+
+use SPC\builder\Extension;
+use SPC\util\CustomExt;
+
+#[CustomExt('dba')]
+class dba extends Extension
+{
+    public function getUnixConfigureArg(): string
+    {
+        $qdbm = $this->builder->getLib('qdbm') ? (' --with-qdbm=' . BUILD_ROOT_PATH) : '';
+        return '--enable-dba' . $qdbm;
+    }
+}
--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -13,13 +13,13 @@ declare(strict_types=1);
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'yac',
-    'Windows' => 'mbstring,pdo_sqlite,mbregex,yac',
+    'Linux', 'Darwin' => 'dba',
+    'Windows' => 'mbstring,pdo_sqlite,mbregex',
 };
 
 // If you want to test lib-suggests feature with extension, add them below (comma separated, example `libwebp,libavif`).
 $with_libs = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => '',
+    'Linux', 'Darwin' => 'qdbm',
     'Windows' => '',
 };
 
