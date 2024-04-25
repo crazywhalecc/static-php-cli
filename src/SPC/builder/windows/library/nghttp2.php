7a604f71be4620c57297a36ccc233c6ed1127235
crazywhalecc <jesse2061@outlook.com>
2024-04-25 14:51:28 +0800

    fix windows nghttp2 dynamic linking bug

--- src/SPC/builder/windows/library/nghttp2.php
+++ src/SPC/builder/windows/library/nghttp2.php
@@ -25,6 +25,8 @@ class nghttp2 extends WindowsLibraryBase
                 '-DCMAKE_BUILD_TYPE=Release ' .
                 '-DENABLE_SHARED_LIB=OFF ' .
                 '-DENABLE_STATIC_LIB=ON ' .
+                '-DBUILD_STATIC_LIBS=ON ' .
+                '-DBUILD_SHARED_LIBS=OFF ' .
                 '-DENABLE_STATIC_CRT=ON ' .
                 '-DENABLE_LIB_ONLY=ON ' .
                 '-DCMAKE_INSTALL_PREFIX=' . BUILD_ROOT_PATH . ' '
