b3018af61cbb16eaf812a7c1e1922c1b4303671b
crazywhalecc <jesse2061@outlook.com>
2024-06-05 13:12:25 +0800

    Fix mongodb support on macOS

--- src/SPC/builder/extension/mongodb.php
+++ src/SPC/builder/extension/mongodb.php
@@ -5,20 +5,11 @@ declare(strict_types=1);
 namespace SPC\builder\extension;
 
 use SPC\builder\Extension;
-use SPC\store\FileSystem;
 use SPC\util\CustomExt;
 
 #[CustomExt('mongodb')]
 class mongodb extends Extension
 {
-    public function patchBeforeBuildconf(): bool
-    {
-        FileSystem::replaceFileStr(SOURCE_PATH . '/php-src/ext/mongodb/config.m4', 'if test -z "$PHP_CONFIG"; then', 'if false; then');
-        FileSystem::replaceFileStr(SOURCE_PATH . '/php-src/ext/mongodb/config.m4', 'PHP_MONGODB_PHP_VERSION=`${PHP_CONFIG} --version`', 'PHP_MONGODB_PHP_VERSION=' . $this->builder->getPHPVersion());
-        FileSystem::replaceFileStr(SOURCE_PATH . '/php-src/ext/mongodb/config.m4', 'PHP_MONGODB_PHP_VERSION_ID=`${PHP_CONFIG} --vernum`', 'PHP_MONGODB_PHP_VERSION_ID=' . $this->builder->getPHPVersionID());
-        return true;
-    }
-
     public function getUnixConfigureArg(): string
     {
         $arg = ' --enable-mongodb ';
