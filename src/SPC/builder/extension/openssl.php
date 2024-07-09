5bd9115a20fe194b7fdbaf4e048a4c281be73898
crazywhalecc <jesse2061@outlook.com>
2024-07-09 18:20:27 +0800

    Fix pre-built openssl cannot find source in openssl ext patch

--- src/SPC/builder/extension/openssl.php
+++ src/SPC/builder/extension/openssl.php
@@ -13,7 +13,7 @@ class openssl extends Extension
     public function patchBeforeMake(): bool
     {
         // patch openssl3 with php8.0 bug
-        if (file_exists(SOURCE_PATH . '/openssl/VERSION.dat') && $this->builder->getPHPVersionID() < 80100) {
+        if ($this->builder->getPHPVersionID() < 80100) {
             $openssl_c = file_get_contents(SOURCE_PATH . '/php-src/ext/openssl/openssl.c');
             $openssl_c = preg_replace('/REGISTER_LONG_CONSTANT\s*\(\s*"OPENSSL_SSLV23_PADDING"\s*.+;/', '', $openssl_c);
             file_put_contents(SOURCE_PATH . '/php-src/ext/openssl/openssl.c', $openssl_c);
