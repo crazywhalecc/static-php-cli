c53f1d947cc7188ce23e4ed34c9989e4baab2cf4
crazywhalecc <jesse2061@outlook.com>
2024-08-11 21:23:05 +0800

    Add tests

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -19,7 +19,7 @@ $upx = true;
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'curl',
+    'Linux', 'Darwin' => 'iconv',
     'Windows' => 'amqp,apcu',
 };
 
