17c27cb7e8345dd1182772c8a24cfecdaf071f10
crazywhalecc <jesse2061@outlook.com>
2024-08-11 21:25:10 +0800

    Add extension test

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -20,7 +20,7 @@ $upx = true;
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
     'Linux', 'Darwin' => 'iconv',
-    'Windows' => 'amqp,apcu',
+    'Windows' => 'amqp,apcu,iconv',
 };
 
 // If you want to test lib-suggests feature with extension, add them below (comma separated, example `libwebp,libavif`).
