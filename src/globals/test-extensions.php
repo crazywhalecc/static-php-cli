29efc2c5a53b839ad50a35d5beaeea8fd41c2331
Jerry Ma <jesse2061@outlook.com>
2024-09-20 12:32:31 +0800

    Add extension gmssl support (#544)
    * Add extension gmssl support
    
    * cs-fix
    
    * Add framework for gmssl

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -19,8 +19,8 @@ $upx = true;
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'redis,igbinary,msgpack',
-    'Windows' => 'redis,igbinary,msgpack',
+    'Linux', 'Darwin' => 'gmssl',
+    'Windows' => 'gmssl',
 };
 
 // If you want to test lib-suggests feature with extension, add them below (comma separated, example `libwebp,libavif`).
