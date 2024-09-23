330dd2bfc6092a3bde6ae0b710b53338d43579da
daddeffe <daddef@gmail.com>
2024-09-23 10:19:40 +0200

    Added optional zlib and disabled two extension (#545)
    asd

--- src/SPC/builder/unix/library/librdkafka.php
+++ src/SPC/builder/unix/library/librdkafka.php
@@ -16,15 +16,19 @@ trait librdkafka
     protected function build(): void
     {
         $builddir = BUILD_ROOT_PATH;
+
+        $zstd_option = $this->builder->getLib('zstd') ? ("STATIC_LIB_libzstd={$builddir}/lib/libzstd.a ") : '';
         shell()->cd($this->source_dir)
             ->exec(
+                $zstd_option .
                 './configure ' .
-                '--enable-static --disable-shared --disable-curl --disable-sasl --disable-valgrind ' .
+                '--enable-static --disable-shared --disable-curl --disable-sasl --disable-valgrind --disable-zlib --disable-ssl ' .
+                ($zstd_option == '' ? '--disable-zstd ' : '') .
                 '--prefix='
             )
             ->exec('make clean')
             ->exec("make -j{$this->builder->concurrency}")
-            ->exec('make install DESTDIR=' . BUILD_ROOT_PATH);
+            ->exec("make install DESTDIR={$builddir}");
         $this->patchPkgconfPrefix(['rdkafka.pc', 'rdkafka-static.pc', 'rdkafka++.pc', 'rdkafka++-static.pc']);
         // remove dynamic libs
         shell()
