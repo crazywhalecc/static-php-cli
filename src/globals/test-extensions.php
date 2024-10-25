ddafdf8987f3747303ef36f940a331baf36b4793
crazywhalecc <jesse2061@outlook.com>
2024-10-25 16:38:02 +0800

    Add PHP 8.4 support (testing)

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -13,10 +13,10 @@ declare(strict_types=1);
 
 // test php version
 $test_php_version = [
-    '8.0',
     '8.1',
     '8.2',
     '8.3',
+    '8.4',
 ];
 
 // test os (macos-13, macos-14, ubuntu-latest, windows-latest are available)
@@ -34,7 +34,7 @@ $no_strip = false;
 $upx = false;
 
 // prefer downloading pre-built packages to speed up the build process
-$prefer_pre_built = true;
+$prefer_pre_built = false;
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
