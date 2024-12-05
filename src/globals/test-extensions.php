fe72b800c6eddc4ad0de4a1d5793b33d334ccdc4
crazywhalecc <jesse2061@outlook.com>
2024-12-05 11:17:37 +0800

    Add pgsql tests

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -38,7 +38,7 @@ $prefer_pre_built = false;
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'grpc,openssl',
+    'Linux', 'Darwin' => 'grpc,openssl,pgsql',
     'Windows' => 'amqp,apcu,bcmath,bz2,calendar,ctype,curl,dba,dom,ds,exif,ffi,fileinfo,filter,ftp,gd,iconv,igbinary,libxml,mbregex,mbstring,mysqli,mysqlnd,opcache,openssl,pdo,pdo_mysql,pdo_sqlite,pdo_sqlsrv,phar,rar,redis,session,shmop,simdjson,simplexml,soap,sockets,sqlite3,sqlsrv,ssh2,swow,sysvshm,tokenizer,xml,xmlreader,xmlwriter,yac,yaml,zip,zlib',
 };
 
