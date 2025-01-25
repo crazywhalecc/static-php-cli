4db00452060ce2e283b32a4748f1c291bbcc9aa0
crazywhalecc <jesse2061@outlook.com>
2025-01-25 15:52:32 +0800

    Disable libdeflate for libtiff

--- src/SPC/builder/unix/library/libtiff.php
+++ src/SPC/builder/unix/library/libtiff.php
@@ -19,8 +19,8 @@ trait libtiff
         $extra_libs = '--enable-zlib --with-zlib-include-dir=' . BUILD_ROOT_PATH . '/include --with-zlib-lib-dir=' . BUILD_ROOT_PATH . '/lib';
         // libjpeg
         $extra_libs .= ' --enable-jpeg --disable-old-jpeg --disable-jpeg12 --with-jpeg-include-dir=' . BUILD_ROOT_PATH . '/include --with-jpeg-lib-dir=' . BUILD_ROOT_PATH . '/lib';
-        // We disabled lzma, zstd, webp by default to reduce the size of the binary
-        $extra_libs .= ' --disable-lzma --disable-zstd --disable-webp';
+        // We disabled lzma, zstd, webp, libdeflate by default to reduce the size of the binary
+        $extra_libs .= ' --disable-lzma --disable-zstd --disable-webp --disable-libdeflate';
 
         $shell = shell()->cd($this->source_dir)
             ->exec(
