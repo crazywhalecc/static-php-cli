9a5f7f68f62175e46eb37a780c53a92225160302
crazywhalecc <jesse2061@outlook.com>
2025-02-06 12:05:41 +0800

    Fix windows static build for opentelemetry

--- src/SPC/builder/extension/opentelemetry.php
+++ src/SPC/builder/extension/opentelemetry.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace SPC\builder\extension;
 
 use SPC\builder\Extension;
+use SPC\store\FileSystem;
 use SPC\util\CustomExt;
 use SPC\util\GlobalEnvManager;
 
@@ -16,6 +17,19 @@ class opentelemetry extends Extension
         if ($this->builder->getPHPVersionID() < 80000 && getenv('SPC_SKIP_PHP_VERSION_CHECK') !== 'yes') {
             throw new \RuntimeException('The opentelemetry extension requires PHP 8.0 or later');
         }
+    }
+
+    public function patchBeforeBuildconf(): bool
+    {
+        if (PHP_OS_FAMILY === 'Windows') {
+            FileSystem::replaceFileStr(
+                SOURCE_PATH . '/php-src/ext/opentelemetry/config.w32',
+                "EXTENSION('opentelemetry', 'opentelemetry.c otel_observer.c', '/DZEND_ENABLE_STATIC_TSRMLS_CACHE=1');",
+                "EXTENSION('opentelemetry', 'opentelemetry.c otel_observer.c', PHP_OPENTELEMETRY_SHARED, '/DZEND_ENABLE_STATIC_TSRMLS_CACHE=1');"
+            );
+            return true;
+        }
+        return false;
     }
 
     public function patchBeforeMake(): bool
