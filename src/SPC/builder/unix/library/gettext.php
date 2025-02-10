b4cdd9e42b0589ee42e1912d38de94299fc14dfd
crazywhalecc <jesse2061@outlook.com>
2025-02-10 18:30:28 +0800

    Fix gettext included with multithreaded bug

--- src/SPC/builder/unix/library/gettext.php
+++ src/SPC/builder/unix/library/gettext.php
@@ -10,14 +10,21 @@ trait gettext
     {
         $extra = $this->builder->getLib('ncurses') ? ('--with-libncurses-prefix=' . BUILD_ROOT_PATH . ' ') : '';
         $extra .= $this->builder->getLib('libxml2') ? ('--with-libxml2-prefix=' . BUILD_ROOT_PATH . ' ') : '';
+
+        $zts = $this->builder->getOption('enable-zts') ? '--enable-threads=isoc+posix ' : '--disable-threads ';
+
+        $cflags = $this->builder->getOption('enable-zts') ? '-lpthread -D_REENTRANT' : '';
+        $ldflags = $this->builder->getOption('enable-zts') ? '-lpthread' : '';
+
         shell()->cd($this->source_dir)
-            ->setEnv(['CFLAGS' => $this->getLibExtraCFlags(), 'LDFLAGS' => $this->getLibExtraLdFlags(), 'LIBS' => $this->getLibExtraLibs()])
+            ->setEnv(['CFLAGS' => $this->getLibExtraCFlags() ?: $cflags, 'LDFLAGS' => $this->getLibExtraLdFlags() ?: $ldflags, 'LIBS' => $this->getLibExtraLibs()])
             ->execWithEnv(
                 './configure ' .
                 '--enable-static ' .
                 '--disable-shared ' .
                 '--disable-java ' .
                 '--disable-c+ ' .
+                $zts .
                 $extra .
                 '--with-included-gettext ' .
                 '--with-libiconv-prefix=' . BUILD_ROOT_PATH . ' ' .
--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -22,9 +22,9 @@ $test_php_version = [
 // test os (macos-13, macos-14, ubuntu-latest, windows-latest are available)
 $test_os = [
     // 'macos-13',
-    // 'macos-14',
-    // 'ubuntu-latest',
-    'windows-latest',
+    'macos-14',
+    'ubuntu-latest',
+    // 'windows-latest',
 ];
 
 // whether enable thread safe
@@ -40,8 +40,8 @@ $prefer_pre_built = false;
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'curl',
-    'Windows' => 'curl',
+    'Linux', 'Darwin' => 'gettext',
+    'Windows' => 'gettext',
 };
 
 // If you want to test lib-suggests feature with extension, add them below (comma separated, example `libwebp,libavif`).
