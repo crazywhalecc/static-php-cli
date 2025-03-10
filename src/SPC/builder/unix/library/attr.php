40ea306008e0650647eb9cc9f32a4cd74082dfa0
Marc Henderkes <m.henderkes@pc-college.de>
2025-03-10 11:42:44 +0100

    try something with static

--- src/SPC/builder/unix/library/attr.php
+++ src/SPC/builder/unix/library/attr.php
@@ -13,9 +13,14 @@ trait attr
      */
     protected function build(): void
     {
+        $cflags = PHP_OS_FAMILY !== 'Linux' ? '-Wimplicit-function-declaration -Wno-int-conversion' : '';
+        $ldflags = PHP_OS_FAMILY !== 'Linux' ? '' : '--static';
         shell()->cd($this->source_dir)
-            ->setEnv(['CFLAGS' => $this->getLibExtraCFlags(), 'LDFLAGS' => $this->getLibExtraLdFlags(), 'LIBS' => $this->getLibExtraLibs()])
-            ->execWithEnv('./autogen.sh')
+            ->setEnv([
+                'CFLAGS' => trim('-I' . BUILD_INCLUDE_PATH . ' ' . $this->getLibExtraCFlags() . ' ' . $cflags),
+                'LDFLAGS' => trim('-L' . BUILD_LIB_PATH . ' ' . $this->getLibExtraLdFlags() . ' ' . $ldflags),
+                'LIBS' => $this->getLibExtraLibs(),
+            ])->execWithEnv('./autogen.sh')
             ->execWithEnv('./configure --prefix= --enable-static --disable-shared')
             ->execWithEnv("make -j {$this->builder->concurrency}")
             ->exec('make install DESTDIR=' . BUILD_ROOT_PATH);
