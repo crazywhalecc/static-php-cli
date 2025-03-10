5b3f44cfca06b6dd7c5c5151f9534c3ff6ad732c
Marc Henderkes <m.henderkes@pc-college.de>
2025-03-10 11:46:53 +0100

    arch c flags

--- src/SPC/builder/unix/library/attr.php
+++ src/SPC/builder/unix/library/attr.php
@@ -13,7 +13,7 @@ trait attr
      */
     protected function build(): void
     {
-        $cflags = PHP_OS_FAMILY !== 'Linux' ? '-Wimplicit-function-declaration -Wno-int-conversion' : '';
+        $cflags = PHP_OS_FAMILY !== 'Linux' ? "{$this->builder->arch_c_flags} -Wimplicit-function-declaration -Wno-int-conversion" : '';
         $ldflags = PHP_OS_FAMILY !== 'Linux' ? '' : '--static';
         shell()->cd($this->source_dir)
             ->setEnv([
