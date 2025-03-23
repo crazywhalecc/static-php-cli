6f502a29d8d411d2619aa6b3fe16ad33b8be645d
crazywhalecc <jesse2061@outlook.com>
2025-03-23 23:04:25 +0800

    Fix linux pdo_odbc patch

--- src/SPC/builder/extension/pdo_odbc.php
+++ src/SPC/builder/extension/pdo_odbc.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace SPC\builder\extension;
 
 use SPC\builder\Extension;
-use SPC\builder\macos\MacOSBuilder;
 use SPC\store\FileSystem;
 use SPC\util\CustomExt;
 
@@ -14,11 +13,8 @@ class pdo_odbc extends Extension
 {
     public function patchBeforeBuildconf(): bool
     {
-        if ($this->builder instanceof MacOSBuilder) {
-            FileSystem::replaceFileStr(SOURCE_PATH . '/php-src/ext/pdo_odbc/config.m4', 'PDO_ODBC_LDFLAGS="$pdo_odbc_def_ldflags', 'PDO_ODBC_LDFLAGS="-liconv $pdo_odbc_def_ldflags');
-            return true;
-        }
-        return false;
+        FileSystem::replaceFileStr(SOURCE_PATH . '/php-src/ext/pdo_odbc/config.m4', 'PDO_ODBC_LDFLAGS="$pdo_odbc_def_ldflags', 'PDO_ODBC_LDFLAGS="-liconv $pdo_odbc_def_ldflags');
+        return true;
     }
 
     public function getUnixConfigureArg(): string
