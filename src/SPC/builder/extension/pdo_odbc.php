d099694ebfaaeac2408326da2535622cddfeaeab
crazywhalecc <jesse2061@outlook.com>
2025-03-23 22:51:21 +0800

    Add iconv for macOS

--- src/SPC/builder/extension/pdo_odbc.php
+++ src/SPC/builder/extension/pdo_odbc.php
@@ -5,11 +5,22 @@ declare(strict_types=1);
 namespace SPC\builder\extension;
 
 use SPC\builder\Extension;
+use SPC\builder\macos\MacOSBuilder;
+use SPC\store\FileSystem;
 use SPC\util\CustomExt;
 
 #[CustomExt('pdo_odbc')]
 class pdo_odbc extends Extension
 {
+    public function patchBeforeBuildconf(): bool
+    {
+        if ($this->builder instanceof MacOSBuilder) {
+            FileSystem::replaceFileStr(SOURCE_PATH . '/php-src/ext/pdo_odbc/config.m4', 'PDO_ODBC_LDFLAGS="$pdo_odbc_def_ldflags', 'PDO_ODBC_LDFLAGS="-liconv $pdo_odbc_def_ldflags');
+            return true;
+        }
+        return false;
+    }
+
     public function getUnixConfigureArg(): string
     {
         return '--with-pdo-odbc=unixODBC,' . BUILD_ROOT_PATH;
