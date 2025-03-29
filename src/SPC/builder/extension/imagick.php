3915c8410b9efda3a9ff9f4f495935c9b8c12504
crazywhalecc <jesse2061@outlook.com>
2025-03-29 23:15:55 +0800

    Destroy imagick config for disabling openmp

--- src/SPC/builder/extension/imagick.php
+++ src/SPC/builder/extension/imagick.php
@@ -5,11 +5,19 @@ declare(strict_types=1);
 namespace SPC\builder\extension;
 
 use SPC\builder\Extension;
+use SPC\store\FileSystem;
 use SPC\util\CustomExt;
 
 #[CustomExt('imagick')]
 class imagick extends Extension
 {
+    public function patchBeforeBuildconf(): bool
+    {
+        // destroy imagick build conf to avoid libgomp build error
+        FileSystem::replaceFileStr(SOURCE_PATH . '/ext/imagick/config.m4', '#include <omp.h>', '#include <NEVER_INCLUDE_OMP_H>');
+        return true;
+    }
+
     public function getUnixConfigureArg(): string
     {
         return '--with-imagick=' . BUILD_ROOT_PATH;
