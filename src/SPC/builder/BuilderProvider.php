6525ef79b91795cc3ca389db4a56e0e2adaffa5e
Yoram <yoramdelangen@gmail.com>
2025-04-15 16:22:13 +0200

    remove builder customizer

--- src/SPC/builder/BuilderProvider.php
+++ src/SPC/builder/BuilderProvider.php
@@ -20,16 +20,6 @@ class BuilderProvider
 {
     private static ?BuilderBase $builder = null;
 
-    private static ?\Closure $customizeBuilder = null;
-
-    /**
-     * @param \Closure(BuilderBase): void $callback
-     */
-    public static function customize(\Closure $callback): void
-    {
-        self::$customizeBuilder = $callback;
-    }
-
     /**
      * @throws FileSystemException
      * @throws RuntimeException
@@ -46,10 +36,7 @@ class BuilderProvider
             'BSD' => new BSDBuilder($input->getOptions()),
             default => throw new WrongUsageException('Current OS "' . PHP_OS_FAMILY . '" is not supported yet'),
         };
-        // allow to add/customize builder instance early
-        if (self::$customizeBuilder) {
-            call_user_func_array(self::$customizeBuilder, [&self::$builder]);
-        }
+
         return self::$builder;
     }
 
