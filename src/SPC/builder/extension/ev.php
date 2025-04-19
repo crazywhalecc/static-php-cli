efec33127fc5625b0209e466265470af5fd1dce3
crazywhalecc <jesse2061@outlook.com>
2025-04-19 16:53:13 +0800

    Fix windows ev patch

---
+++ src/SPC/builder/extension/ev.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SPC\builder\extension;
+
+use SPC\builder\Extension;
+use SPC\exception\FileSystemException;
+use SPC\store\FileSystem;
+use SPC\util\CustomExt;
+
+#[CustomExt('ev')]
+class ev extends Extension
+{
+    /**
+     * @throws FileSystemException
+     */
+    public function patchBeforeBuildconf(): bool
+    {
+        /*
+         * replace EXTENSION('ev', php_ev_sources, true, ' /DZEND_ENABLE_STATIC_TSRMLS_CACHE=1');
+         * to EXTENSION('ev', php_ev_sources, PHP_EV_SHARED, ' /DZEND_ENABLE_STATIC_TSRMLS_CACHE=1');
+         */
+        FileSystem::replaceFileLineContainsString(
+            $this->source_dir . '/config.w32',
+            'EXTENSION(\'ev\'',
+            "		EXTENSION('ev', php_ev_sources, PHP_EV_SHARED, ' /DZEND_ENABLE_STATIC_TSRMLS_CACHE=1');"
+        );
+        sleep(3);
+        return true;
+    }
+}
