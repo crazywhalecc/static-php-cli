a3ee3e5a24be0c2a944db9cdcc97ebc633a01a4a
DubbleClick <m@pyc.ac>
2025-05-06 16:41:35 +0700

    add to tests

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -45,7 +45,7 @@ $prefer_pre_built = false;
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'pgsql',
+    'Linux', 'Darwin' => 'xsl,simplexml,xslwriter',
     'Windows' => 'xlswriter,openssl',
 };
 
