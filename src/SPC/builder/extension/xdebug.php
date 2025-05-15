1ae27c7b5a7292d8b5f5246812c41db2be6bdd7d
Marc <m@pyc.ac>
2025-05-16 00:24:18 +0700

    oopsie

--- src/SPC/builder/extension/xdebug.php
+++ src/SPC/builder/extension/xdebug.php
@@ -13,7 +13,7 @@ class xdebug extends Extension
 {
     public function runSharedExtensionCheckUnix(): void
     {
-        [$ret, $out] = shell()->execWithResult(BUILD_BIN_PATH . '/php -v');
+        [$ret, $out] = shell()->execWithResult(BUILD_BIN_PATH . '/php -n -d "zend_extension=' . BUILD_MODULES_PATH . '/xdebug.so" -v');
         if ($ret !== 0) {
             throw new RuntimeException('xdebug.so failed to load.');
         }
