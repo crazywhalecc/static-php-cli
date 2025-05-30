2a5915f4b609110a7b23531f3380e61f8acb9062
DubbleClick <m@pyc.ac>
2025-05-30 21:21:50 +0700

    patched this in ext-spx before, do it here instead

--- src/SPC/builder/extension/spx.php
+++ src/SPC/builder/extension/spx.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace SPC\builder\extension;
 
 use SPC\builder\Extension;
+use SPC\store\FileSystem;
 use SPC\util\CustomExt;
 
 #[CustomExt('spx')]
@@ -17,5 +18,15 @@ class spx extends Extension
             $arg .= ' --with-zlib-dir=' . BUILD_ROOT_PATH;
         }
         return $arg;
+    }
+
+    public function patchBeforeConfigure(): bool
+    {
+        FileSystem::replaceFileStr(
+            $this->source_dir . '/Makefile.frag',
+            '@cp -r assets/web-ui/*',
+            '@cp -r ' . $this->source_dir . '/assets/web-ui/*',
+        );
+        return true;
     }
 }
