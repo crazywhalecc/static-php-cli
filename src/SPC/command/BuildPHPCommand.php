55b4ba5832bb9f51d3dfc94f1ed8e50c30f94092
DubbleClick <m@pyc.ac>
2025-06-01 13:21:42 +0700

    fix file_exists check in printing shared extension folder

--- src/SPC/command/BuildPHPCommand.php
+++ src/SPC/command/BuildPHPCommand.php
@@ -251,7 +251,7 @@ class BuildPHPCommand extends BuildCommand
             if (!empty($shared_extensions)) {
                 foreach ($shared_extensions as $ext) {
                     $path = FileSystem::convertPath("{$build_root_path}/modules/{$ext}.so");
-                    if (file_exists("{$build_root_path}/modules/{$ext}.so")) {
+                    if (file_exists(BUILD_MODULES_PATH . "/{$ext}.so")) {
                         logger()->info("Shared extension [{$ext}] path{$fixed}: {$path}");
                     } else {
                         logger()->warning("Shared extension [{$ext}] not found, please check!");
