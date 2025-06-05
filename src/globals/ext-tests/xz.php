e2191113b0b09073c80a3db7cc3c7c4b54f7d1db
DubbleClick <m@pyc.ac>
2025-06-05 16:42:52 +0700

    tests for brotli and xz

---
+++ src/globals/ext-tests/xz.php
@@ -0,0 +1,8 @@
+﻿<?php
+
+declare(strict_types=1);
+
+$str = 'Data you would like compressed.';
+assert(function_exists('xzencode'));
+assert(function_exists('xzdecode'));
+assert(xzdecode(xzencode($str)) === $str);
