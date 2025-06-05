57b527c88660be49de3be71a0f3dd69dabea08fc
DubbleClick <m@pyc.ac>
2025-06-05 21:29:20 +0700

    gzencode, of course

--- src/globals/ext-tests/zlib.php
+++ src/globals/ext-tests/zlib.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-assert(function_exists('gzcompress'));
+assert(function_exists('gzencode'));
 assert(function_exists('gzdecode'));
 
 $input = str_repeat('The quick brown fox jumps over the lazy dog. ', 10);
-$compressed = gzcompress($input);
+$compressed = gzencode($input);
 assert(is_string($compressed));
 assert(strlen($compressed) < strlen($input));
 
