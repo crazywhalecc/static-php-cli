17b4c18830ae2d156c330c78c54d2d83f6007ae1
DubbleClick <m@pyc.ac>
2025-06-05 19:59:45 +0700

    accidental space

--- src/globals/ext-tests/brotli.php
+++ src/globals/ext-tests/brotli.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 
 $str = 'brotli_compress ( string $data, int $level = BROTLI_COMPRESS_LEVEL_DEFAULT, int $mode = BROTLI_GENERIC, string|null $dict = null ): string|false';
 assert(function_exists('brotli_compress'));
-assert(function_exists('brotli_uncompress '));
+assert(function_exists('brotli_uncompress'));
 assert(brotli_uncompress(brotli_compress($str)) === $str);
