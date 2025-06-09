aedcc86fc5a99d6b0123a541535a7980c8aa4fc9
DubbleClick <m@pyc.ac>
2025-06-09 15:52:17 +0700

    fix cmake minimum version for libtidy

--- src/SPC/builder/unix/library/tidy.php
+++ src/SPC/builder/unix/library/tidy.php
@@ -16,13 +16,16 @@ trait tidy
      */
     protected function build(): void
     {
-        UnixCMakeExecutor::create($this)
+        $cmake = UnixCMakeExecutor::create($this)
             ->setBuildDir("{$this->source_dir}/build-dir")
             ->addConfigureArgs(
                 '-DSUPPORT_CONSOLE_APP=OFF',
                 '-DBUILD_SHARED_LIB=OFF'
-            )
-            ->build();
+            );
+        if (version_compare(get_cmake_version(), '4.0.0', '>=')) {
+            $cmake->addConfigureArgs('-DCMAKE_POLICY_VERSION_MINIMUM=3.5');
+        }
+        $cmake->build();
         $this->patchPkgconfPrefix(['tidy.pc']);
     }
 }
