8466970a1fe413dedf7ec3dede376f857ad05733
crazywhalecc <jesse2061@outlook.com>
2025-06-10 19:07:16 +0800

    Add tests

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -21,15 +21,15 @@ $test_php_version = [
 
 // test os (macos-13, macos-14, macos-15, ubuntu-latest, windows-latest are available)
 $test_os = [
-    'macos-13',
+    // 'macos-13',
     // 'macos-14',
-    'macos-15',
-    'ubuntu-latest',
-    'ubuntu-22.04',
+    // 'macos-15',
+    // 'ubuntu-latest',
+    // 'ubuntu-22.04',
     // 'ubuntu-24.04',
-    'ubuntu-22.04-arm',
+    // 'ubuntu-22.04-arm',
     // 'ubuntu-24.04-arm',
-    // 'windows-latest',
+    'windows-latest',
 ];
 
 // whether enable thread safe
@@ -46,7 +46,7 @@ $prefer_pre_built = false;
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
     'Linux', 'Darwin' => 'curl',
-    'Windows' => 'xlswriter,openssl',
+    'Windows' => 'intl',
 };
 
 // If you want to test shared extensions, add them below (comma separated, example `bcmath,openssl`).
@@ -57,7 +57,7 @@ $shared_extensions = match (PHP_OS_FAMILY) {
 
 // If you want to test lib-suggests feature with extension, add them below (comma separated, example `libwebp,libavif`).
 $with_libs = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'brotli,curl,freetype,gmssl,libaom,libavif,libde265,libevent,libheif,libjpeg,librabbitmq,libssh2,libuuid,libuv,libwebp,libxml2,libyaml,libzip,mimalloc,snappy,tidy,zstd',
+    'Linux', 'Darwin' => '',
     'Windows' => '',
 };
 
