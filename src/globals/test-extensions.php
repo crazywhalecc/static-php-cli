28e5e584b49d06573c63b003ec93bcd71630e197
crazywhalecc <jesse2061@outlook.com>
2025-06-12 01:20:02 +0800

    Revert ext-matrix-tests and test-extensions

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -21,14 +21,14 @@ $test_php_version = [
 
 // test os (macos-13, macos-14, macos-15, ubuntu-latest, windows-latest are available)
 $test_os = [
-    // 'macos-13',
+    'macos-13',
     // 'macos-14',
     'macos-15',
     // 'ubuntu-latest',
-    // 'ubuntu-22.04',
-    // 'ubuntu-24.04',
-    // 'ubuntu-22.04-arm',
-    // 'ubuntu-24.04-arm',
+    'ubuntu-22.04',
+    'ubuntu-24.04',
+    'ubuntu-22.04-arm',
+    'ubuntu-24.04-arm',
     // 'windows-latest',
 ];
 
@@ -45,7 +45,7 @@ $prefer_pre_built = false;
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'dom',
+    'Linux', 'Darwin' => 'dom,mongodb',
     'Windows' => 'xlswriter,openssl',
 };
 
