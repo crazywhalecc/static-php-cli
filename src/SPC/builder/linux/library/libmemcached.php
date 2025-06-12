089b94d7535398f6a70ad8ff6c1010fe7265f92c
DubbleClick <m@pyc.ac>
2025-06-12 10:46:26 +0700

    add fastlz, libmemcached for linux

--- src/SPC/builder/linux/library/libmemcached.php
+++ src/SPC/builder/linux/library/libmemcached.php
@@ -4,17 +4,14 @@ declare(strict_types=1);
 
 namespace SPC\builder\linux\library;
 
-use SPC\exception\RuntimeException;
+use SPC\util\executor\UnixCMakeExecutor;
 
-/**
- * gmp is a template library class for unix
- */
 class libmemcached extends LinuxLibraryBase
 {
     public const NAME = 'libmemcached';
 
-    public function build()
+    public function build(): void
     {
-        throw new RuntimeException('libmemcached is currently not supported on Linux platform');
+        UnixCMakeExecutor::create($this)->build();
     }
 }
--- src/SPC/builder/macos/library/libmemcached.php
+++ src/SPC/builder/macos/library/libmemcached.php
@@ -4,17 +4,14 @@ declare(strict_types=1);
 
 namespace SPC\builder\macos\library;
 
-use SPC\util\executor\UnixAutoconfExecutor;
+use SPC\util\executor\UnixCMakeExecutor;
 
-/**
- * gmp is a template library class for unix
- */
 class libmemcached extends MacOSLibraryBase
 {
     public const NAME = 'libmemcached';
 
     public function build(): void
     {
-        UnixAutoconfExecutor::create($this)->configure('--disable-sasl')->exec("sed -ie 's/-Werror//g' ./Makefile")->make();
+        UnixCMakeExecutor::create($this)->build();
     }
 }
