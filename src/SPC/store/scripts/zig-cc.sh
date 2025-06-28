de3cb2f68686b01f7e28281e312d8de74db666ad
DubbleClick <m@pyc.ac>
2025-06-28 21:59:12 +0700

    no ""

--- src/SPC/store/scripts/zig-cc.sh
+++ src/SPC/store/scripts/zig-cc.sh
@@ -61,14 +61,14 @@ if [ "$SPC_LIBC" = "glibc" ]; then
 fi
 
 if [ "$SPC_TARGET_WAS_SET" -eq 0 ] && [ -z "$SPC_LIBC" ] && [ -z "$SPC_LIBC_VERSION" ]; then
-    exec zig cc "${COMPILER_EXTRA}" "${PARSED_ARGS[@]}"
+    exec zig cc ${COMPILER_EXTRA} "${PARSED_ARGS[@]}"
 elif [ -z "$SPC_LIBC" ] && [ -z "$SPC_LIBC_VERSION" ]; then
-    exec zig cc -target "${SPC_TARGET}" "${COMPILER_EXTRA}" "${PARSED_ARGS[@]}"
+    exec zig cc -target "${SPC_TARGET}" ${COMPILER_EXTRA} "${PARSED_ARGS[@]}"
 else
     TARGET="${SPC_TARGET}-${SPC_LIBC}"
     [ -n "$SPC_LIBC_VERSION" ] && TARGET="${TARGET}.${SPC_LIBC_VERSION}"
 
-    output=$(zig cc -target "$TARGET" -lstdc++ "${COMPILER_EXTRA}" "${PARSED_ARGS[@]}" 2>&1)
+    output=$(zig cc -target "$TARGET" -lstdc++ ${COMPILER_EXTRA} "${PARSED_ARGS[@]}" 2>&1)
     status=$?
 
     if [ $status -eq 0 ]; then
@@ -78,16 +78,16 @@ else
 
     if echo "$output" | grep -q "version '.*' in target triple"; then
         TARGET_FALLBACK="${SPC_TARGET}-${SPC_LIBC}"
-        output=$(zig cc -target "$TARGET_FALLBACK" -lstdc++ "${COMPILER_EXTRA}" "${PARSED_ARGS[@]}" 2>&1)
+        output=$(zig cc -target "$TARGET_FALLBACK" -lstdc++ ${COMPILER_EXTRA} "${PARSED_ARGS[@]}" 2>&1)
         status=$?
 
         if [ $status -eq 0 ]; then
             echo "$output"
             exit 0
         else
-            exec zig cc -target "$TARGET_FALLBACK" "${COMPILER_EXTRA}" "${PARSED_ARGS[@]}"
+            exec zig cc -target "$TARGET_FALLBACK" ${COMPILER_EXTRA} "${PARSED_ARGS[@]}"
         fi
     else
-        exec zig cc -target "$TARGET" "${COMPILER_EXTRA}" "${PARSED_ARGS[@]}"
+        exec zig cc -target "$TARGET" ${COMPILER_EXTRA} "${PARSED_ARGS[@]}"
     fi
 fi
