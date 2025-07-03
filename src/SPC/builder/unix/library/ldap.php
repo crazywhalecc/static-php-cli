aff3373b78dc670ad7b778ba9b303f383cda2fd4
DubbleClick <m@pyc.ac>
2025-07-03 11:11:21 +0700

    remote -lutil?

--- src/SPC/builder/unix/library/ldap.php
+++ src/SPC/builder/unix/library/ldap.php
@@ -12,7 +12,7 @@ trait ldap
 {
     public function patchBeforeBuild(): bool
     {
-        $extra = SPCTarget::getLibc() === 'glibc' ? '-ldl -lpthread -lm -lresolv -lutil' : '';
+        $extra = SPCTarget::getLibc() === 'glibc' ? '-ldl -lpthread -lm -lresolv' : '';
         FileSystem::replaceFileStr($this->source_dir . '/configure', '"-lssl -lcrypto', '"-lssl -lcrypto -lz ' . $extra);
         return true;
     }
