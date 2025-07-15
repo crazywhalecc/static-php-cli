f0612591be642557eda8fe719eee58f966c6cdb0
crazywhalecc <jesse2061@outlook.com>
2025-07-15 23:31:26 +0800

    Test bulk (without amqp, swoole) and spc-max

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -16,7 +16,7 @@ $test_php_version = [
     // '8.1',
     // '8.2',
     // '8.3',
-    // '8.4',
+    '8.4',
     '8.5',
 ];
 
@@ -25,12 +25,12 @@ $test_os = [
     'macos-13', // bin/spc for x86_64
     // 'macos-14',  // bin/spc for arm64
     'macos-15', // bin/spc for arm64
-    // 'ubuntu-latest', // bin/spc-alpine-docker for x86_64
-    'ubuntu-22.04', // bin/spc-gnu-docker for x86_64
-    'ubuntu-24.04', // bin/spc for x86_64
-    // 'ubuntu-22.04-arm', // bin/spc-gnu-docker for arm64
-    // 'ubuntu-24.04-arm', // bin/spc for arm64
-    // 'windows-latest', // .\bin\spc.ps1
+    'ubuntu-latest', // bin/spc-alpine-docker for x86_64
+    // 'ubuntu-22.04', // bin/spc-gnu-docker for x86_64
+    // 'ubuntu-24.04', // bin/spc for x86_64
+    'ubuntu-22.04-arm', // bin/spc-gnu-docker for arm64
+    'ubuntu-24.04-arm', // bin/spc for arm64
+    'windows-latest', // .\bin\spc.ps1
 ];
 
 // whether enable thread safe
@@ -49,8 +49,8 @@ $prefer_pre_built = false;
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'imap,swoole',
-    'Windows' => 'intl',
+    'Linux', 'Darwin' => 'apcu,bcmath,bz2,calendar,ctype,curl,dba,dom,event,exif,fileinfo,filter,ftp,gd,gmp,iconv,imagick,imap,intl,mbregex,mbstring,mysqli,mysqlnd,opcache,openssl,opentelemetry,pcntl,pdo,pdo_mysql,pgsql,phar,posix,protobuf,readline,redis,session,shmop,simplexml,soap,sockets,sodium,sqlite3,sysvmsg,sysvsem,sysvshm,tokenizer,xml,xmlreader,xmlwriter,xsl,zip,zlib',
+    'Windows' => 'apcu,bcmath,bz2,calendar,ctype,curl,dba,dom,ds,exif,ffi,fileinfo,filter,ftp,gd,iconv,igbinary,libxml,mbregex,mbstring,mysqli,mysqlnd,opcache,openssl,pdo,pdo_mysql,pdo_sqlite,pdo_sqlsrv,phar,rar,redis,session,shmop,simdjson,simplexml,soap,sockets,sqlite3,sqlsrv,ssh2,sysvshm,tokenizer,xml,xmlreader,xmlwriter,yac,yaml,zip,zlib',
 };
 
 // If you want to test shared extensions, add them below (comma separated, example `bcmath,openssl`).
@@ -73,7 +73,7 @@ $with_libs = match (PHP_OS_FAMILY) {
 // You can use `common`, `bulk`, `minimal` or `none`.
 // note: combination is only available for *nix platform. Windows must use `none` combination
 $base_combination = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'bulk',
+    'Linux', 'Darwin' => 'none',
     'Windows' => 'none',
 };
 
