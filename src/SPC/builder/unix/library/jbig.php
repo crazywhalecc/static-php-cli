be0dbcd9ebefc65480c294d41498a191a990dc9b
DubbleClick <m@pyc.ac>
2025-07-18 14:04:55 +0700

    add jbig

---
+++ src/SPC/builder/unix/library/jbig.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SPC\builder\unix\library;
+
+use SPC\exception\FileSystemException;
+use SPC\exception\RuntimeException;
+use SPC\store\FileSystem;
+
+trait jbig
+{
+    /**
+     * @throws FileSystemException
+     * @throws RuntimeException
+     */
+    protected function build(): void
+    {
+        // Patch Makefile to add -fPIC flag for position-independent code
+        FileSystem::replaceFileStr($this->source_dir . '/Makefile', 'CFLAGS = -O2 -W -Wno-unused-result', 'CFLAGS = -O2 -W -Wno-unused-result -fPIC');
+
+        // Build the library
+        shell()->cd($this->source_dir)->initializeEnv($this)
+            ->exec("make -j{$this->builder->concurrency} {$this->builder->getEnvString()} lib")
+            ->exec('cp libjbig/libjbig.a ' . BUILD_LIB_PATH)
+            ->exec('cp libjbig/libjbig85.a ' . BUILD_LIB_PATH)
+            ->exec('cp libjbig/jbig.h ' . BUILD_INCLUDE_PATH)
+            ->exec('cp libjbig/jbig85.h ' . BUILD_INCLUDE_PATH)
+            ->exec('cp libjbig/jbig_ar.h ' . BUILD_INCLUDE_PATH);
+    }
+}
