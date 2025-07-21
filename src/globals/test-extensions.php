8eb77231143a1fff79b8b1f5142bb246f810ffb7
DubbleClick <m@pyc.ac>
2025-07-21 09:46:06 +0700

    enable tests

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -21,15 +21,15 @@ $test_php_version = [
 
 // test os (macos-13, macos-14, macos-15, ubuntu-latest, windows-latest are available)
 $test_os = [
-    // 'macos-13', // bin/spc for x86_64
-    // 'macos-14',  // bin/spc for arm64
-    // 'macos-15', // bin/spc for arm64
-    // 'ubuntu-latest', // bin/spc-alpine-docker for x86_64
-    // 'ubuntu-22.04', // bin/spc-gnu-docker for x86_64
-    // 'ubuntu-24.04', // bin/spc for x86_64
-    // 'ubuntu-22.04-arm', // bin/spc-gnu-docker for arm64
-    // 'ubuntu-24.04-arm', // bin/spc for arm64
-    'windows-latest', // .\bin\spc.ps1
+    'macos-13', // bin/spc for x86_64
+    'macos-14',  // bin/spc for arm64
+    'macos-15', // bin/spc for arm64
+    'ubuntu-latest', // bin/spc-alpine-docker for x86_64
+    'ubuntu-22.04', // bin/spc-gnu-docker for x86_64
+    'ubuntu-24.04', // bin/spc for x86_64
+    'ubuntu-22.04-arm', // bin/spc-gnu-docker for arm64
+    'ubuntu-24.04-arm', // bin/spc for arm64
+    // 'windows-latest', // .\bin\spc.ps1
 ];
 
 // whether enable thread safe
@@ -48,7 +48,7 @@ $prefer_pre_built = false;
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'imap,swoole',
+    'Linux', 'Darwin' => 'grpc',
     'Windows' => 'curl',
 };
 
