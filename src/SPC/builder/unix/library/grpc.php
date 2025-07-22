c5811ae9473996aec9131f8085a3fda9f16343f7
crazywhalecc <jesse2061@outlook.com>
2025-07-22 14:46:28 +0800

    Reduce grpc building things

--- src/SPC/builder/unix/library/grpc.php
+++ src/SPC/builder/unix/library/grpc.php
@@ -26,12 +26,18 @@ trait grpc
         $cmake = UnixCMakeExecutor::create($this)
             ->setBuildDir("{$this->source_dir}/avoid_BUILD_file_conflict")
             ->addConfigureArgs(
-                '-DgRPC_SSL_PROVIDER=package',
                 '-DgRPC_INSTALL_BINDIR=' . BUILD_BIN_PATH,
                 '-DgRPC_INSTALL_LIBDIR=' . BUILD_LIB_PATH,
                 '-DgRPC_INSTALL_SHAREDIR=' . BUILD_ROOT_PATH . '/share/grpc',
                 '-DCMAKE_C_FLAGS="-DGRPC_POSIX_FORK_ALLOW_PTHREAD_ATFORK -L' . BUILD_LIB_PATH . ' -I' . BUILD_INCLUDE_PATH . '"',
-                '-DCMAKE_CXX_FLAGS="-DGRPC_POSIX_FORK_ALLOW_PTHREAD_ATFORK -L' . BUILD_LIB_PATH . ' -I' . BUILD_INCLUDE_PATH . '"'
+                '-DCMAKE_CXX_FLAGS="-DGRPC_POSIX_FORK_ALLOW_PTHREAD_ATFORK -L' . BUILD_LIB_PATH . ' -I' . BUILD_INCLUDE_PATH . '"',
+                '-DgRPC_BUILD_CODEGEN=OFF',
+                '-DgRPC_DOWNLOAD_ARCHIVES=OFF',
+                '-DgRPC_BUILD_TESTS=OFF',
+                // providers
+                '-DgRPC_ZLIB_PROVIDER=package',
+                '-DgRPC_CARES_PROVIDER=package',
+                '-DgRPC_SSL_PROVIDER=package',
             );
 
         if (PHP_OS_FAMILY === 'Linux' && SPCTarget::isStatic() && !SystemUtil::isMuslDist()) {
