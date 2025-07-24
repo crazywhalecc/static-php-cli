b3f1103143db1376c1a9a075bead45710e64de7b
DubbleClick <m@pyc.ac>
2025-07-24 14:28:34 +0700

    well shit, zig cannot build libjxl

--- src/SPC/builder/unix/library/libjxl.php
+++ src/SPC/builder/unix/library/libjxl.php
@@ -12,17 +12,19 @@ trait libjxl
     protected function build(): void
     {
         UnixCMakeExecutor::create($this)
-            ->addConfigureArgs('-DJPEGXL_ENABLE_TOOLS=OFF')
-            ->addConfigureArgs('-DJPEGXL_ENABLE_EXAMPLES=OFF')
-            ->addConfigureArgs('-DJPEGXL_ENABLE_MANPAGES=OFF')
-            ->addConfigureArgs('-DJPEGXL_ENABLE_BENCHMARK=OFF')
-            ->addConfigureArgs('-DJPEGXL_ENABLE_PLUGINS=OFF')
-            ->addConfigureArgs('-DJPEGXL_ENABLE_SJPOEG=ON')
-            ->addConfigureArgs('-DJPEGXL_ENABLE_JNI=OFF')
-            ->addConfigureArgs('-DJPEGXL_ENABLE_TRANSCODE_JPEG=ON')
-            ->addConfigureArgs('-DJPEGXL_STATIC=' . (SPCTarget::isStatic() ? 'ON' : 'OFF'))
-            ->addConfigureArgs('-DJPEGXL_FORCE_SYSTEM_BROTLI=ON')
-            ->addConfigureArgs('-DBUILD_TESTING=OFF')
+            ->addConfigureArgs(
+                '-DJPEGXL_ENABLE_TOOLS=OFF',
+                '-DJPEGXL_ENABLE_EXAMPLES=OFF',
+                '-DJPEGXL_ENABLE_MANPAGES=OFF',
+                '-DJPEGXL_ENABLE_BENCHMARK=OFF',
+                '-DJPEGXL_ENABLE_PLUGINS=OFF',
+                '-DJPEGXL_ENABLE_SJPOEG=ON',
+                '-DJPEGXL_ENABLE_JNI=OFF',
+                '-DJPEGXL_ENABLE_TRANSCODE_JPEG=ON',
+                '-DJPEGXL_STATIC=' . (SPCTarget::isStatic() ? 'ON' : 'OFF'),
+                '-DJPEGXL_FORCE_SYSTEM_BROTLI=ON',
+                '-DBUILD_TESTING=OFF'
+            )
             ->build();
     }
 }
