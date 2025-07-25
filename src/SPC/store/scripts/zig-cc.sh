ea56e460d0b74967dcb9acf5066f2e9b98a2c350
DubbleClick <m@pyc.ac>
2025-07-25 10:00:49 +0700

    zig script

--- src/SPC/store/scripts/zig-cc.sh
+++ src/SPC/store/scripts/zig-cc.sh
@@ -19,6 +19,13 @@ while [[ $# -gt 0 ]]; do
             ARG_ABS="$(realpath "$ARG" 2>/dev/null || true)"
             [[ "$ARG_ABS" == "$BUILDROOT_ABS" ]] && PARSED_ARGS+=("-I$ARG") || PARSED_ARGS+=("-isystem$ARG")
             ;;
+        -march=*|-mcpu=*) # replace -march=x86-64 with -march=x86_64
+            OPT_NAME="${1%%=*}"
+            OPT_VALUE="${1#*=}"
+            OPT_VALUE="${OPT_VALUE//-/_}"
+            PARSED_ARGS+=("${OPT_NAME}=${OPT_VALUE}")
+            shift
+            ;;
         *)
             PARSED_ARGS+=("$1")
             shift
