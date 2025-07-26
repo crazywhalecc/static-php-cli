fb62a0e3382d7efdba3f2771c1e489edd8e2fc97
DubbleClick <m@pyc.ac>
2025-07-26 20:12:18 +0700

    small adjustments

--- src/SPC/toolchain/ZigToolchain.php
+++ src/SPC/toolchain/ZigToolchain.php
@@ -15,8 +15,8 @@ class ZigToolchain implements ToolchainInterface
         // Set environment variables for zig toolchain
         GlobalEnvManager::putenv('SPC_LINUX_DEFAULT_CC=zig-cc');
         GlobalEnvManager::putenv('SPC_LINUX_DEFAULT_CXX=zig-c++');
-        GlobalEnvManager::putenv('SPC_LINUX_DEFAULT_AR=ar');
-        GlobalEnvManager::putenv('SPC_LINUX_DEFAULT_LD=ld');
+        GlobalEnvManager::putenv('SPC_LINUX_DEFAULT_AR=zig ar');
+        GlobalEnvManager::putenv('SPC_LINUX_DEFAULT_LD=zig-cc');
 
         // Generate additional object needed for zig toolchain
         $paths = ['/usr/lib/gcc', '/usr/local/lib/gcc'];
