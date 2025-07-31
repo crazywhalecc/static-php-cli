c58bc038f4ed50d87e0add34d70ee263903707db
DubbleClick <m@pyc.ac>
2025-07-31 21:11:42 +0700

    version 2.7.0

--- src/SPC/ConsoleApplication.php
+++ src/SPC/ConsoleApplication.php
@@ -34,7 +34,7 @@ use Symfony\Component\Console\Application;
  */
 final class ConsoleApplication extends Application
 {
-    public const VERSION = '2.6.1';
+    public const VERSION = '2.7.0';
 
     public function __construct()
     {
