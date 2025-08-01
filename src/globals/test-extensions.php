1c9fbdb032524d868987bf33f27e6c6e6bf5e1d5
crazywhalecc <jesse2061@outlook.com>
2025-08-01 15:01:55 +0800

    Use none base combination

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -73,7 +73,7 @@ $with_libs = match (PHP_OS_FAMILY) {
 // You can use `common`, `bulk`, `minimal` or `none`.
 // note: combination is only available for *nix platform. Windows must use `none` combination
 $base_combination = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'common',
+    'Linux', 'Darwin' => 'none',
     'Windows' => 'none',
 };
 
