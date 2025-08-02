c3e9a2816bfbc11f57e49eab08c5dfc5963a23e5
crazywhalecc <jesse2061@outlook.com>
2025-08-03 00:29:13 +0800

    Test

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -16,9 +16,9 @@ $test_php_version = [
     // '8.1',
     // '8.2',
     // '8.3',
-    // '8.4',
+    '8.4',
     // '8.5',
-    'git',
+    // 'git',
 ];
 
 // test os (macos-13, macos-14, macos-15, ubuntu-latest, windows-latest are available)
@@ -26,10 +26,10 @@ $test_os = [
     // 'macos-13', // bin/spc for x86_64
     // 'macos-14',  // bin/spc for arm64
     // 'macos-15', // bin/spc for arm64
-    // 'ubuntu-latest', // bin/spc-alpine-docker for x86_64
+    'ubuntu-latest', // bin/spc-alpine-docker for x86_64
     // 'ubuntu-22.04', // bin/spc-gnu-docker for x86_64
     // 'ubuntu-24.04', // bin/spc for x86_64
-    // 'ubuntu-22.04-arm', // bin/spc-gnu-docker for arm64
+    'ubuntu-22.04-arm', // bin/spc-gnu-docker for arm64
     'ubuntu-24.04-arm', // bin/spc for arm64
     // 'windows-latest', // .\bin\spc.ps1
 ];
