afd67910750dde1346d7ab33675c1bca9c12a6a0
crazywhalecc <jesse2061@outlook.com>
2025-08-06 21:02:14 +0800

    Fix phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,8 @@ require_once __DIR__ . '/../src/globals/internal-env.php';
 require_once __DIR__ . '/mock/SPC_store.php';
 
 \SPC\util\AttributeMapper::init();
+
+$log_dir = SPC_LOGS_DIR;
+if (!file_exists($log_dir)) {
+    mkdir($log_dir, 0755, true);
+}
