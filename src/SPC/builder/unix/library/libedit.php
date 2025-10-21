2e13be2a7aded02e93f802ab58d9619fb3e28030
henderkes <m@pyc.ac>
2025-10-21 13:54:34 +0200

    prevent libedit from building strlcat/strlcpy, no idea why their soure code doesn't prevent this correctly itself

--- src/SPC/builder/unix/library/libedit.php
+++ src/SPC/builder/unix/library/libedit.php
@@ -4,16 +4,27 @@ declare(strict_types=1);
 
 namespace SPC\builder\unix\library;
 
+use SPC\store\FileSystem;
 use SPC\util\executor\UnixAutoconfExecutor;
 
 trait libedit
 {
     protected function build(): void
     {
-        UnixAutoconfExecutor::create($this)
+        $make = UnixAutoconfExecutor::create($this)
             ->appendEnv(['CFLAGS' => '-D__STDC_ISO_10646__=201103L'])
-            ->configure()
-            ->make();
+            ->configure();
+
+        foreach (['strlcpy', 'strlcat', 'fgetln'] as $symbol) {
+            $usymbol = strtoupper($symbol);
+            FileSystem::replaceFileLineContainsString(
+                $this->source_dir . '/config.h',
+                "/* #undef HAVE_{$usymbol} */",
+                "/* #undef HAVE_{$usymbol} */\n#define {$symbol} libedit_{$symbol}"
+            );
+        }
+
+        $make->make();
         $this->patchPkgconfPrefix(['libedit.pc']);
     }
 }
