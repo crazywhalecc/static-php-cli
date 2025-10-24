c30b34ae5c6f56b3aef0eb5fd885e7dec4e44024
crazywhalecc <jesse2061@outlook.com>
2025-10-24 17:02:15 +0800

    Fix gettext missing symbols on macOS

--- src/SPC/builder/extension/gettext.php
+++ src/SPC/builder/extension/gettext.php
@@ -15,7 +15,7 @@ class gettext extends Extension
     public function patchBeforeBuildconf(): bool
     {
         if ($this->builder instanceof MacOSBuilder) {
-            FileSystem::replaceFileStr(SOURCE_PATH . '/php-src/ext/gettext/config.m4', 'AC_CHECK_LIB($GETTEXT_CHECK_IN_LIB', 'AC_CHECK_LIB(intl');
+            FileSystem::replaceFileStr(SOURCE_PATH . '/php-src/ext/gettext/config.m4', 'AC_CHECK_LIB([$GETTEXT_CHECK_IN_LIB', 'AC_CHECK_LIB([intl');
         }
         return true;
     }
