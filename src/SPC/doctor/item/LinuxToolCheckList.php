b8854f14899faee89cabb10f20779c5011369dab
crazywhalecc <jesse2061@outlook.com>
2025-10-26 11:23:16 +0800

    Add file

--- src/SPC/doctor/item/LinuxToolCheckList.php
+++ src/SPC/doctor/item/LinuxToolCheckList.php
@@ -40,7 +40,7 @@ class LinuxToolCheckList
         'tar', 'unzip', 'gzip', 'gcc', 'g++',
         'bzip2', 'cmake', 'patch', 'which',
         'xz', 'libtool', 'gettext-devel',
-        'patchelf',
+        'patchelf', 'file',
     ];
 
     public const TOOLS_ARCH = [
