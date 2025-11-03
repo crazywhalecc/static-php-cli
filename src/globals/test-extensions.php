589a4a9803c2b31be9da632160ffdaeae920b4d0
henderkes <m@pyc.ac>
2025-11-03 21:13:47 +0100

    test rdkafka with all suggested libs

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -13,11 +13,11 @@ declare(strict_types=1);
 
 // test php version (8.1 ~ 8.4 available, multiple for matrix)
 $test_php_version = [
-    '8.1',
-    '8.2',
-    '8.3',
+    // '8.1',
+    // '8.2',
+    // '8.3',
     '8.4',
-    '8.5',
+    // '8.5',
     // 'git',
 ];
 
@@ -62,7 +62,7 @@ $shared_extensions = match (PHP_OS_FAMILY) {
 };
 
 // If you want to test lib-suggests for all extensions and libraries, set it to true.
-$with_suggested_libs = false;
+$with_suggested_libs = true;
 
 // If you want to test extra libs for extensions, add them below (comma separated, example `libwebp,libavif`). Unnecessary, when $with_suggested_libs is true.
 $with_libs = match (PHP_OS_FAMILY) {
