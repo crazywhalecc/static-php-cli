1fed8f28022a85ad0631e6270574b2a6e26d80f9
Marc Henderkes <m.henderkes@pc-college.de>
2025-11-18 17:22:27 +0100

    add required libs to krb5

--- src/SPC/builder/unix/library/krb5.php
+++ src/SPC/builder/unix/library/krb5.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace SPC\builder\unix\library;
 
 use SPC\util\executor\UnixAutoconfExecutor;
+use SPC\util\SPCConfigUtil;
 
 trait krb5
 {
@@ -12,8 +13,12 @@ trait krb5
     {
         $this->source_dir .= '/src';
         shell()->cd($this->source_dir)->exec('autoreconf -if');
+        $libs = array_map(fn ($x) => $x->getName(), $this->getDependencies(true));
+        $spc = new SPCConfigUtil($this->builder, ['no_php' => true, 'libs_only_deps' => true]);
+        $config = $spc->config(libraries: $libs, include_suggest_lib: $this->builder->getOption('with-suggested-libs', false));
         UnixAutoconfExecutor::create($this)
             ->appendEnv([
+                'LIBS' => $config['libs'],
                 'LDFLAGS' => '-Wl,--allow-multiple-definition',
             ])
             ->optionalLib('ldap', '--with-ldap', '--without-ldap')
