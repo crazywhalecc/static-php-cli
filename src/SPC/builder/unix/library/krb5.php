84e9f13688afd163baf8954ce63cc7a46dd18b31
henderkes <m@pyc.ac>
2025-11-19 14:48:10 +0100

    framework?

--- src/SPC/builder/unix/library/krb5.php
+++ src/SPC/builder/unix/library/krb5.php
@@ -16,16 +16,15 @@ trait krb5
         $libs = array_map(fn ($x) => $x->getName(), $this->getDependencies(true));
         $spc = new SPCConfigUtil($this->builder, ['no_php' => true, 'libs_only_deps' => true]);
         $config = $spc->config(libraries: $libs, include_suggest_lib: $this->builder->getOption('with-suggested-libs', false));
-        $make = UnixAutoconfExecutor::create($this)
-            ->appendEnv([
-                'CFLAGS' => '-fcommon',
-                'LIBS' => $config['libs'],
-            ]);
+        $extraEnv = [
+            'CFLAGS' => '-fcommon',
+            'LIBS' => $config['libs'],
+        ];
         if (getenv('SPC_LD_LIBRARY_PATH') && getenv('SPC_LIBRARY_PATH')) {
-            $make->appendEnv([
+            $extraEnv = [...$extraEnv, ...[
                 'LD_LIBRARY_PATH' => getenv('SPC_LD_LIBRARY_PATH'),
                 'LIBRARY_PATH' => getenv('SPC_LIBRARY_PATH'),
-            ]);
+            ]];
         }
         $args = [
             '--disable-nls',
@@ -33,9 +32,11 @@ trait krb5
             '--without-system-verto',
         ];
         if (PHP_OS_FAMILY === 'Darwin') {
+            $extraEnv['LDFLAGS'] = '-framework Kerberos';
             $args[] = 'ac_cv_func_secure_getenv=no';
         }
-        $make
+        UnixAutoconfExecutor::create($this)
+            ->appendEnv($extraEnv)
             ->optionalLib('ldap', '--with-ldap', '--without-ldap')
             ->optionalLib('libedit', '--with-libedit', '--without-libedit')
             ->configure(...$args)
