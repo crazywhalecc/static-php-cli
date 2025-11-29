f6c818d3c0af2cceba8cd142af71b0441238eda6
crazywhalecc <jesse2061@outlook.com>
2025-11-29 18:58:16 +0800

    v3 base

--- src/SPC/ConsoleApplication.php
+++ src/SPC/ConsoleApplication.php
@@ -34,7 +34,7 @@ use Symfony\Component\Console\Application;
  */
 final class ConsoleApplication extends Application
 {
-    public const string VERSION = '2.7.9';
+    public const string VERSION = '3.0.0-dev';
 
     public function __construct()
     {
