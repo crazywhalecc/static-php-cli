14b822a185379b62c9daae3259bccd320942fcd7
henderkes <m@pyc.ac>
2025-12-01 16:55:52 +0100

    don't build avx2 if we don't have it

--- src/SPC/builder/unix/library/libwebp.php
+++ src/SPC/builder/unix/library/libwebp.php
@@ -10,8 +10,13 @@ trait libwebp
 {
     protected function build(): void
     {
+        $cflags = getenv('SPC_DEFAULT_C_FLAGS') ?: getenv('CFLAGS') ?: '';
+        $has_avx2 = str_contains($cflags, '-mavx2') || str_contains($cflags, '-march=x86-64-v2') || str_contains($cflags, '-march=x86-64-v3');
         UnixCMakeExecutor::create($this)
-            ->addConfigureArgs('-DWEBP_BUILD_EXTRAS=ON')
+            ->addConfigureArgs(
+                '-DWEBP_BUILD_EXTRAS=ON',
+                '-DWEBP_ENABLE_SIMD=' . ($has_avx2 ? 'ON' : 'OFF'),
+            )
             ->build();
         // patch pkgconfig
         $this->patchPkgconfPrefix(patch_option: PKGCONF_PATCH_PREFIX | PKGCONF_PATCH_LIBDIR);
