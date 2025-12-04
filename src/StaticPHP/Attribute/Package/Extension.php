20892ab1940df01243242017321dee8e29bbe714
crazywhalecc <jesse2061@outlook.com>
2025-12-04 21:19:55 +0800

    Auto-append prefix for php-extension packages

--- src/StaticPHP/Attribute/Package/Extension.php
+++ src/StaticPHP/Attribute/Package/Extension.php
@@ -8,7 +8,12 @@ namespace StaticPHP\Attribute\Package;
  * Indicates that the annotated class defines a PHP extension.
  */
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
-readonly class Extension
+class Extension
 {
-    public function __construct(public string $name) {}
+    public function __construct(public string $name)
+    {
+        if (!str_starts_with($name, 'ext-')) {
+            $this->name = "ext-{$name}";
+        }
+    }
 }
