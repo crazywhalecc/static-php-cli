d3b98837b6415b0b7eef47224b9d26f171756afb
crazywhalecc <jesse2061@outlook.com>
2025-12-08 10:57:51 +0800

    Add Zig package support with downloader and installation checks

---
+++ src/StaticPHP/Doctor/Item/ZigCheck.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StaticPHP\Doctor\Item;
+
+use StaticPHP\Attribute\Doctor\CheckItem;
+use StaticPHP\Attribute\Doctor\FixItem;
+use StaticPHP\Attribute\Doctor\OptionalCheck;
+use StaticPHP\DI\ApplicationContext;
+use StaticPHP\Doctor\CheckResult;
+use StaticPHP\Package\PackageInstaller;
+use StaticPHP\Toolchain\Interface\ToolchainInterface;
+use StaticPHP\Toolchain\ZigToolchain;
+
+#[OptionalCheck([self::class, 'optionalCheck'])]
+class ZigCheck
+{
+    public static function optionalCheck(): bool
+    {
+        return ApplicationContext::get(ToolchainInterface::class) instanceof ZigToolchain;
+    }
+
+    /** @noinspection PhpUnused */
+    #[CheckItem('if zig is installed', level: 800)]
+    public function checkZig(): CheckResult
+    {
+        $installer = new PackageInstaller();
+        $package = 'zig';
+        $installer->addInstallPackage($package);
+        $installed = $installer->isPackageInstalled($package);
+        if ($installed) {
+            return CheckResult::ok();
+        }
+        return CheckResult::fail('zig is not installed', 'install-zig');
+    }
+
+    #[FixItem('install-zig')]
+    public function installZig(): bool
+    {
+        $arch = arch2gnu(php_uname('m'));
+        $os = match (PHP_OS_FAMILY) {
+            'Windows' => 'win',
+            'Darwin' => 'macos',
+            'BSD' => 'freebsd',
+            default => 'linux',
+        };
+        $installer = new PackageInstaller();
+        $installer->addInstallPackage('zig');
+        $installer->run(false);
+        return $installer->isPackageInstalled('zig');
+    }
+}
