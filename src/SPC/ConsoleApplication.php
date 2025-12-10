3c89ce6c7f834d66c65e47c24724d6619c41a0fd
Jerry Ma <jesse2061@outlook.com>
2025-12-10 17:14:27 +0800

    Update version to 2.7.10 (#997)

--- src/SPC/ConsoleApplication.php
+++ src/SPC/ConsoleApplication.php
@@ -34,7 +34,7 @@ use Symfony\Component\Console\Application;
  */
 final class ConsoleApplication extends Application
 {
-    public const string VERSION = '2.7.9';
+    public const string VERSION = '2.7.10';
 
     public function __construct()
     {
