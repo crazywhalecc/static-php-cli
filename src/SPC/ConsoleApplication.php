8ce3997f3028f6fd35eb9c7976aa825bc9e2c08a
Jerry Ma <jesse2061@outlook.com>
2025-12-18 20:18:30 +0800

    Update version number to 2.7.11

--- src/SPC/ConsoleApplication.php
+++ src/SPC/ConsoleApplication.php
@@ -34,7 +34,7 @@ use Symfony\Component\Console\Application;
  */
 final class ConsoleApplication extends Application
 {
-    public const string VERSION = '2.7.10';
+    public const string VERSION = '2.7.11';
 
     public function __construct()
     {
