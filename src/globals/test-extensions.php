e7a88f1df76956297d19db09dcd3e22812162aa7
henderkes <m@pyc.ac>
2025-12-29 21:15:53 +0100

    enable fat for gmp when next version releases

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -50,7 +50,7 @@ $prefer_pre_built = false;
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'mysqli',
+    'Linux', 'Darwin' => 'mysqli,gmp',
     'Windows' => 'bcmath',
 };
 
