3fa2d69813662e495af801e9f3b51cee87c6634a
crazywhalecc <jesse2061@outlook.com>
2026-02-04 16:24:57 +0800

    Add ext-mbstring,ext-mbregex,onig

--- src/Package/Library/onig.php
+++ src/Package/Library/onig.php
@@ -7,6 +7,7 @@ namespace Package\Library;
 use StaticPHP\Attribute\Package\BuildFor;
 use StaticPHP\Attribute\Package\Library;
 use StaticPHP\Package\LibraryPackage;
+use StaticPHP\Runtime\Executor\UnixAutoconfExecutor;
 use StaticPHP\Runtime\Executor\WindowsCMakeExecutor;
 use StaticPHP\Util\FileSystem;
 
@@ -20,5 +21,13 @@ class onig
             ->addConfigureArgs('-DMSVC_STATIC_RUNTIME=ON')
             ->build();
         FileSystem::copy("{$package->getLibDir()}\\onig.lib", "{$package->getLibDir()}\\onig_a.lib");
+    }
+
+    #[BuildFor('Linux')]
+    #[BuildFor('Darwin')]
+    public function buildUnix(LibraryPackage $lib): void
+    {
+        UnixAutoconfExecutor::create($lib)->configure()->make();
+        $lib->patchPkgconfPrefix(['oniguruma.pc']);
     }
 }
