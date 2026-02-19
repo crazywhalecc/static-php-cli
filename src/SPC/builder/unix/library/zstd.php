f717ae9d01f4c553d790774a422c62e4bdfbe640
henderkes <m@pyc.ac>
2026-02-19 22:17:23 +0700

    need to build zstd static

--- src/SPC/builder/unix/library/zstd.php
+++ src/SPC/builder/unix/library/zstd.php
@@ -13,7 +13,7 @@ trait zstd
         UnixCMakeExecutor::create($this)
             ->setBuildDir("{$this->source_dir}/build/cmake/build")
             ->addConfigureArgs(
-                '-DZSTD_BUILD_STATIC=' . (getenv('SPC_STATIC_LIBS') ? 'ON' : 'OFF'),
+                '-DZSTD_BUILD_STATIC=ON', // otherwise zstd util fails to build
                 '-DZSTD_BUILD_SHARED='. (getenv('SPC_STATIC_LIBS') ? 'OFF' : 'ON'),
             )
             ->build();
