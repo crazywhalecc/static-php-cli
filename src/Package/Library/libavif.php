d316684995d32dee5d16b2c2eca86fb21822df08
crazywhalecc <jesse2061@outlook.com>
2026-02-28 10:37:38 +0800

    Add optional package support for libaom, libsharpyuv, libjpeg, libxml2, and libpng in Unix build

--- src/Package/Library/libavif.php
+++ src/Package/Library/libavif.php
@@ -17,6 +17,11 @@ class libavif
     public function buildUnix(LibraryPackage $lib): void
     {
         UnixCMakeExecutor::create($lib)
+            ->optionalPackage('libaom', '-DAVIF_CODEC_AOM=SYSTEM', '-DAVIF_CODEC_AOM=OFF')
+            ->optionalPackage('libsharpyuv', '-DAVIF_LIBSHARPYUV=SYSTEM', '-DAVIF_LIBSHARPYUV=OFF')
+            ->optionalPackage('libjpeg', '-DAVIF_JPEG=SYSTEM', '-DAVIF_JPEG=OFF')
+            ->optionalPackage('libxml2', '-DAVIF_LIBXML2=SYSTEM', '-DAVIF_LIBXML2=OFF')
+            ->optionalPackage('libpng', '-DAVIF_LIBPNG=SYSTEM', '-DAVIF_LIBPNG=OFF')
             ->addConfigureArgs('-DAVIF_LIBYUV=OFF')
             ->build();
         // patch pkgconfig
