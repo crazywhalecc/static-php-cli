520767638f779dd7cfffb38666c6f6dfa9615b67
crazywhalecc <jesse2061@outlook.com>
2026-03-21 11:20:05 +0800

    Add gmssl

--- src/Package/Library/gmssl.php
+++ src/Package/Library/gmssl.php
@@ -8,6 +8,8 @@ use StaticPHP\Attribute\Package\BuildFor;
 use StaticPHP\Attribute\Package\Library;
 use StaticPHP\Package\LibraryPackage;
 use StaticPHP\Runtime\Executor\UnixCMakeExecutor;
+use StaticPHP\Runtime\Executor\WindowsCMakeExecutor;
+use StaticPHP\Util\FileSystem;
 
 #[Library('gmssl')]
 class gmssl
@@ -17,5 +19,36 @@ class gmssl
     public function build(LibraryPackage $lib): void
     {
         UnixCMakeExecutor::create($lib)->build();
+    }
+
+    #[BuildFor('Windows')]
+    public function buildWin(LibraryPackage $lib): void
+    {
+        $buildDir = "{$lib->getSourceDir()}\\builddir";
+
+        // GmSSL requires NMake Makefiles generator on Windows
+        WindowsCMakeExecutor::create($lib)
+            ->setBuildDir($buildDir)
+            ->setCustomDefaultArgs(
+                '-G "NMake Makefiles"',
+                '-DWIN32=ON',
+                '-DBUILD_SHARED_LIBS=OFF',
+                '-DCMAKE_BUILD_TYPE=Release',
+                '-DCMAKE_C_FLAGS_RELEASE="/MT /O2 /Ob2 /DNDEBUG"',
+                '-DCMAKE_CXX_FLAGS_RELEASE="/MT /O2 /Ob2 /DNDEBUG"',
+                '-DCMAKE_INSTALL_PREFIX=' . escapeshellarg($lib->getBuildRootPath()),
+                '-B ' . escapeshellarg($buildDir),
+            )
+            ->toStep(1)
+            ->build();
+
+        // fix cmake_install.cmake install prefix (GmSSL overrides it internally)
+        $installCmake = "{$buildDir}\\cmake_install.cmake";
+        FileSystem::writeFile(
+            $installCmake,
+            'set(CMAKE_INSTALL_PREFIX "' . str_replace('\\', '/', $lib->getBuildRootPath()) . '")' . PHP_EOL . FileSystem::readFile($installCmake)
+        );
+
+        cmd()->cd($buildDir)->exec('nmake install XCFLAGS=/MT');
     }
 }
