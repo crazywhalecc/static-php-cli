fcd0052d12ceac15c146a5659dee944eb297e4fc
crazywhalecc <jesse2061@outlook.com>
2026-03-24 12:31:31 +0800

    Add sqlite

--- src/Package/Library/sqlite.php
+++ src/Package/Library/sqlite.php
@@ -6,8 +6,11 @@ namespace Package\Library;
 
 use StaticPHP\Attribute\Package\BuildFor;
 use StaticPHP\Attribute\Package\Library;
+use StaticPHP\Attribute\Package\PatchBeforeBuild;
 use StaticPHP\Package\LibraryPackage;
 use StaticPHP\Runtime\Executor\UnixAutoconfExecutor;
+use StaticPHP\Runtime\SystemTarget;
+use StaticPHP\Util\FileSystem;
 
 #[Library('sqlite')]
 class sqlite
@@ -18,5 +21,19 @@ class sqlite
     {
         UnixAutoconfExecutor::create($lib)->configure()->make();
         $lib->patchPkgconfPrefix(['sqlite3.pc']);
+    }
+
+    #[PatchBeforeBuild]
+    public function patchBeforeBuild(LibraryPackage $lib): void
+    {
+        spc_skip_if(SystemTarget::getTargetOS() !== 'Windows', 'This patch is only for Windows builds.');
+        FileSystem::copy(ROOT_DIR . '/src/globals/extra/Makefile-sqlite', "{$lib->getSourceDir()}\\Makefile");
+    }
+
+    #[BuildFor('Windows')]
+    public function buildWin(LibraryPackage $lib): void
+    {
+        cmd()->cd($lib->getSourceDir())
+            ->exec("nmake PREFIX={$lib->getBuildRootPath()} install-static");
     }
 }
