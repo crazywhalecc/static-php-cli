1f42f1a479c10b07ffd6aa77840b04315bef31e0
crazywhalecc <jesse2061@outlook.com>
2026-03-24 10:52:13 +0800

    Add libzip

--- src/Package/Library/libzip.php
+++ src/Package/Library/libzip.php
@@ -8,6 +8,8 @@ use StaticPHP\Attribute\Package\BuildFor;
 use StaticPHP\Attribute\Package\Library;
 use StaticPHP\Package\LibraryPackage;
 use StaticPHP\Runtime\Executor\UnixCMakeExecutor;
+use StaticPHP\Runtime\Executor\WindowsCMakeExecutor;
+use StaticPHP\Util\FileSystem;
 
 #[Library('libzip')]
 class libzip
@@ -32,5 +34,29 @@ class libzip
             )
             ->build();
         $lib->patchPkgconfPrefix(['libzip.pc'], PKGCONF_PATCH_PREFIX);
+    }
+
+    #[BuildFor('Windows')]
+    public function buildWin(LibraryPackage $lib): void
+    {
+        WindowsCMakeExecutor::create($lib)
+            ->optionalPackage('bzip2', ...cmake_boolean_args('ENABLE_BZIP2'))
+            ->optionalPackage('xz', ...cmake_boolean_args('ENABLE_LZMA'))
+            ->optionalPackage('openssl', ...cmake_boolean_args('ENABLE_OPENSSL'))
+            ->optionalPackage('zstd', ...cmake_boolean_args('ENABLE_ZSTD'))
+            ->addConfigureArgs(
+                '-DENABLE_GNUTLS=OFF',
+                '-DENABLE_MBEDTLS=OFF',
+                '-DBUILD_DOC=OFF',
+                '-DBUILD_EXAMPLES=OFF',
+                '-DBUILD_REGRESS=OFF',
+                '-DBUILD_TOOLS=OFF',
+                '-DBUILD_OSSFUZZ=OFF',
+            )
+            ->build();
+        FileSystem::copy(
+            $lib->getBuildRootPath() . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'zip.lib',
+            $lib->getBuildRootPath() . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'libzip_a.lib'
+        );
     }
 }
