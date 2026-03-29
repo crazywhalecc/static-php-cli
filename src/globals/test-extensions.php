8f7897e13bc7dd9dfe488e723bc12f02bf644ad2
henderkes <m@pyc.ac>
2026-03-30 01:06:31 +0700

    test

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -24,11 +24,11 @@ $test_php_version = [
 // test os (macos-15-intel, macos-15, ubuntu-latest, windows-latest are available)
 $test_os = [
     // 'macos-15-intel', // bin/spc for x86_64
-    // 'macos-15', // bin/spc for arm64
-    // 'ubuntu-latest', // bin/spc-alpine-docker for x86_64
-    // 'ubuntu-22.04', // bin/spc-gnu-docker for x86_64
+    'macos-15', // bin/spc for arm64
+    'ubuntu-latest', // bin/spc-alpine-docker for x86_64
+    'ubuntu-22.04', // bin/spc-gnu-docker for x86_64
     // 'ubuntu-24.04', // bin/spc for x86_64
-    // 'ubuntu-22.04-arm', // bin/spc-gnu-docker for arm64
+    'ubuntu-22.04-arm', // bin/spc-gnu-docker for arm64
     // 'ubuntu-24.04-arm', // bin/spc for arm64
     // 'windows-2022', // .\bin\spc.ps1
     'windows-2025',
@@ -42,15 +42,15 @@ $no_strip = false;
 // compress with upx
 $upx = false;
 
-// whether to test frankenphp build, only available for macos and linux
-$frankenphp = false;
+// whether to test frankenphp build, only available for macOS and linux
+$frankenphp = true;
 
 // prefer downloading pre-built packages to speed up the build process
 $prefer_pre_built = false;
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'pgsql',
+    'Linux', 'Darwin' => 'zlib',
     'Windows' => 'gd,zlib,mbstring,filter',
 };
 
@@ -66,7 +66,7 @@ $with_suggested_libs = true;
 
 // If you want to test extra libs for extensions, add them below (comma separated, example `libwebp,libavif`). Unnecessary, when $with_suggested_libs is true.
 $with_libs = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => '',
+    'Linux', 'Darwin' => 'libjpeg',
     'Windows' => '',
 };
 
