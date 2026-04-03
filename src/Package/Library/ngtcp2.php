852a0437bdec3b3127e9cc04b48144ff3ace4f8b
crazywhalecc <jesse2061@outlook.com>
2026-04-03 11:03:22 +0800

    Disable openssl for ngtcp2 temporarily

--- src/Package/Library/ngtcp2.php
+++ src/Package/Library/ngtcp2.php
@@ -24,7 +24,7 @@ class ngtcp2
                 '-DBUILD_SHARED_LIBS=OFF',
                 '-DENABLE_STATIC_CRT=ON',
                 '-DENABLE_LIB_ONLY=ON',
-                '-DENABLE_OPENSSL=ON',
+                '-DENABLE_OPENSSL=OFF',
             )
             ->build();
     }
