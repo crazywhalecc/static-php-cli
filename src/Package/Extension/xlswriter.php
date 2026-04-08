76fc5abfe7ac9d2596822af706cba8b35976b9c6
crazywhalecc <jesse2061@outlook.com>
2026-04-08 10:04:07 +0800

    Apply v2 patch for xlswriter

--- src/Package/Extension/xlswriter.php
+++ src/Package/Extension/xlswriter.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Package\Extension;
 
+use Package\Target\php;
+use StaticPHP\Attribute\Package\BeforeStage;
 use StaticPHP\Attribute\Package\CustomPhpConfigureArg;
 use StaticPHP\Attribute\Package\Extension;
+use StaticPHP\Attribute\PatchDescription;
 use StaticPHP\Package\PackageInstaller;
 use StaticPHP\Package\PhpExtensionPackage;
+use StaticPHP\Util\SourcePatcher;
 
 #[Extension('xlswriter')]
 class xlswriter extends PhpExtensionPackage
@@ -21,5 +25,18 @@ class xlswriter extends PhpExtensionPackage
             $arg .= ' --with-openssl=' . $installer->getLibraryPackage('openssl')->getBuildRootPath();
         }
         return $arg;
+    }
+
+    #[BeforeStage('php', [php::class, 'makeForWindows'], 'ext-xlswriter')]
+    #[PatchDescription('Fix Windows build: apply win32 patch and add UTF-8 BOM to theme.c')]
+    public function patchBeforeMakeForWindows(): void
+    {
+        // fix windows build with openssl extension duplicate symbol bug
+        SourcePatcher::patchFile('spc_fix_xlswriter_win32.patch', $this->getSourceDir());
+        $content = file_get_contents($this->getSourceDir() . '/library/libxlsxwriter/src/theme.c');
+        $bom = pack('CCC', 0xEF, 0xBB, 0xBF);
+        if (!str_starts_with($content, $bom)) {
+            file_put_contents($this->getSourceDir() . '/library/libxlsxwriter/src/theme.c', $bom . $content);
+        }
     }
 }
