39a975dc90c9a9a980982bb8b8b36cff31882da6
crazywhalecc <jesse2061@outlook.com>
2026-04-12 01:12:17 +0800

    Just skip sleep

--- src/StaticPHP/Package/PackageInstaller.php
+++ src/StaticPHP/Package/PackageInstaller.php
@@ -154,10 +154,10 @@ class PackageInstaller
             $this->resolvePackages();
         }
 
-        if ($this->interactive && !$disable_delay_msg) {
-            // show install or build options in terminal with beautiful output
-            $this->printInstallerInfo();
+        // show install or build options in terminal with beautiful output
+        $this->printInstallerInfo();
 
+        if ($this->interactive && !$disable_delay_msg) {
             InteractiveTerm::notice('Build process will start after 2s ...' . PHP_EOL);
             sleep(2);
         }
